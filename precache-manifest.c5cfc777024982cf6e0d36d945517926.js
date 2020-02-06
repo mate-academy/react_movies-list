ad9078c933b404111ac32066a93002ab5ed07c9c
mgrinko <glukym@gmail.com>
2020-02-06 09:47:55 +0200

    Updates

---
+++ precache-manifest.c5cfc777024982cf6e0d36d945517926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d66289af22b05312edc836088674f65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43d5c409613f96e19c81",
+    "url": "./static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "2af0fbb19fee38e98f1a",
+    "url": "./static/css/main.995370ef.chunk.css"
+  },
+  {
+    "revision": "43d5c409613f96e19c81",
+    "url": "./static/js/2.622e6c67.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.622e6c67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2af0fbb19fee38e98f1a",
+    "url": "./static/js/main.1c1d3ee4.chunk.js"
+  },
+  {
+    "revision": "bd651092f6acbe222378",
+    "url": "./static/js/runtime-main.14f95850.js"
+  }
+]);
