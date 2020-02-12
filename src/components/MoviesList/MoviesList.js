cb93e061e9243f739040e4018c5daf7c090c295e
Olexander <kravchenko.sania@gmail.com>
2020-02-12 17:00:59 +0200

    attempt 1

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ list }) => (
+  <>
+    {list.map(item => (
+      <MovieCard
+        title={item.title}
+        description={item.description}
+        imgUrl={item.imgUrl}
+        imdbUrl={item.imdbUrl}
+        imdbId={item.imdbId}
+      />
+    ))}
+  </>
 );
+
+MoviesList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ),
+};
+
+MoviesList.defaultProps = {
+  list: [],
+};
