c37168dd063424da29f88af948588ece536f93e0
Natalia Lypovyk <cardinnata@gmail.com>
2020-02-13 01:27:04 +0200

    Movies List

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ cards }) => (
+  <div className="movies">
+    {cards.map(card => (
+      <MovieCard key={card.imdbId} {...card} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
