4e26c1570d6068ea0c72827b1cfb4ebc93a8957a
Arimazz <bodyaprymyak@icloud.com>
2020-02-12 16:33:03 +0200

    done

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ MoviesArrApi }) => (
+  <>
+    <div className="movies">
+      {MoviesArrApi.map(movie => (
+        <MovieCard {...movie} />
+      ))}
+    </div>
+  </>
 );
+
+MoviesList.propTypes = {
+  MoviesArrApi: PropTypes.arrayOf.isRequired,
+};
