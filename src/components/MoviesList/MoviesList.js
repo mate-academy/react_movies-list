4907227dbada8d8fbc151864296b1d08c7bf18e1
SahaPylypyuk <sashapulupyk@gmail.com>
2020-02-13 08:53:04 +0200

    Task implemented

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ filmes }) => (
+  <>
+    { filmes.map(element => (
+      <MovieCard {...element} />
+    ))}
+
+  </>
 );
+
+MoviesList.propTypes = {
+  filmes: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      imgUrl: PropTypes.string.isRequired,
+      imdbUrl: PropTypes.string.isRequired,
+      imdbId: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
