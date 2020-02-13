08dfe1cd222ca4f4e8d9418d8b9d1502aa15c407
Senya Volkov <formobileprima@gmail.com>
2020-02-13 14:15:14 +0200

    fixed remarks3

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,48 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './MovieCard.scss';
 
-export const MovieCard = ({ card }) => (
-  <>
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={card.imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
+export const MovieCard = ({ card }) => {
+  const { imgUrl, title, imdbUrl, description } = card;
 
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img src={card.imdbUrl} alt="imdb" />
-            </figure>
-          </div>
-
-          <div className="media-content">
-            <p className="title is-8">{card.title}</p>
-          </div>
+  return (
+    <>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={imgUrl}
+              alt="Film logo"
+            />
+          </figure>
         </div>
 
-        <div className="content">
-          {card.description}
-          <br />
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="./images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
 
-          <a href={`https://www.imdb.com/title/${card.imdbId}`}>IMDB</a>
+            <div className="media-content">
+              <p className="title is-8">{title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {description}
+            <br />
+
+            <a href={imdbUrl}>IMDB</a>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 MovieCard.propTypes = {
   card: PropTypes.shape({
-    imgUrl: PropTypes.string,
-    imdbUrl: PropTypes.string,
-    imdbId: PropTypes.string,
-    title: PropTypes.string,
+    imgUrl: PropTypes.string.isRequired,
     description: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    imdbUrl: PropTypes.string.isRequired,
   }).isRequired,
 };
