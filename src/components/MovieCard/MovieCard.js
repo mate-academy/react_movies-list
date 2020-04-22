0f06fbb667b30ffe0e2d065439ddf2f8ce1b57ed
Yarik1993 <makarov1603@icloud.com>
2020-04-22 19:17:16 +0300

    solution

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ moviesInfo }) => (
+  <>
+    {moviesInfo.map(el => (
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={el.imgUrl}
+              alt="Film logo"
+            />
+          </figure>
+        </div>
+
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="/images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8">{el.title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {el.description}
+            <br />
+            <a href={el.imdbUrl}>IMDB</a>
+          </div>
+        </div>
+      </div>
+    ))}
+  </>
 );
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    <MovieCard moviesInfo={movies} />
+  </div>
 );
