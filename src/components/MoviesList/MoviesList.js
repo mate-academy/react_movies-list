ec7d7b7a99b65d2f14330a8bb9c5d3ad8b03cca7
Andrey Faychak <faychak333@gmail.com>
2020-04-22 18:02:45 +0300

    'solution1'

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './MoviesList.scss';
 import {MovieCard} from "../MovieCard";
 
-export const MoviesList = ({movies}) => {
-  return (
-    <div className="movies">
-      {movies.map(el => <MovieCard movie={el}/>)}
-    </div>
-  )
-};
+export const MoviesList = ({movies}) => (
+  <div className="movies">
+    {movies.map(el => <MovieCard movie={el}/>)}
+  </div>
+);
