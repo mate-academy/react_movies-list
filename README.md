9cb35887f5cd75c1225c45e6b3e3f930c91d6296
Andreas_Just <andreas.wq@gmail.com>
2020-04-22 23:59:40 +0300

    Solution

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Movies list
-- Replace `<your_account>` with your Github username in the
- [DEMO LINK](https://<your_account>.github.io/react_movies-list/)
+- Replace `<your_account>` with your Github username in the links
+
+    - [DEMO LINK](https://andreas-just.github.io/react_decompose/)
+    - [PULL REQUEST](https://github.com/mate-academy/react_decompose/pull/127)
+
 - Follow the [React task guideline](https://github.com/mate-academy/react_task-guideline#react-tasks-guideline)
 
 ## Task
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,51 @@
 import React from 'react';
 import './MovieCard.scss';
+import PropTypes from 'prop-types';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+const MovieCard = ({ movie }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={movie.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="/images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{movie.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {movie.description}
+        <br />
+        <a href={movie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
+
+MovieCard.defaultProps = {
+  movie: {},
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    imgUrl: PropTypes.string,
+    imdbUrl: PropTypes.string,
+  }),
+};
+
+export default MovieCard;
