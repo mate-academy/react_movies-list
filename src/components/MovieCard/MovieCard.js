489d31ce2e5075b8aa44a974f1e50d1901901936
ShapovalDenys <denys.igorevich@gmail.com>
2020-04-22 17:19:46 +0300

    solution v0.1

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = prop => (
+  <>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={prop.filminfo.imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+          <div className="media-content">
+            <p className="title is-8">{prop.filminfo.title}</p>
+          </div>
+        </div>
+        <div className="content">
+          {prop.filminfo.description}
+          <br />
+          <a href={prop.filminfo.ImdbUrl}>IMDB</a>
+        </div>
+      </div>
+    </div>
+  </>
 );
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = prop => (
+  <>
+    <div className="movies">
+      {prop.filmInfoList.map(filminfo => (
+        <div key={filminfo.imdbId}>
+          <MovieCard filminfo={filminfo} />
+        </div>
+      ))}
+    </div>
+  </>
 );
