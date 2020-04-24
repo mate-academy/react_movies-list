6a2155205f677a08522ae7d776a0727b25e80bda
Nina <pinkangelssugar@gmail.com>
2020-04-24 13:42:08 +0300

    Nina

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ data }) => (
+  <div className="movies">
+    {data.map(movieInfo => (
+      <MovieCard key={movieInfo.id} info={movieInfo} />))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
