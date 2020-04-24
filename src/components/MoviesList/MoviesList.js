b0c6cad24c3901864059cad31b86f748651d69b8
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-04-24 12:53:40 +0300

    fix

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -6,11 +6,8 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MoviesList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <div className="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
-      </div>
+      <MovieCard {...movie} key={movie.imdbId} />
     ))}
-
   </div>
 );
 
