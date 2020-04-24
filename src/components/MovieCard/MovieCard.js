6a229e0a51769606e09d457673b15a4593101a6f
crash2bash <sachagurov@gmail.com>
2020-04-24 12:56:34 +0300

    fix

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,20 +2,12 @@ import React from 'react';
 import './MovieCard.scss';
 import PropTypes from 'prop-types';
 
-export const MovieCard = ({
-  info:
-                              {
-                                title,
-                                description,
-                                imgUrl,
-                                imdbUrl,
-                              },
-}) => (
+export const MovieCard = ({ info }) => (
   <div className="card">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
-          src={imgUrl}
+          src={info.imgUrl}
           alt="Film logo"
         />
       </figure>
@@ -25,19 +17,19 @@ export const MovieCard = ({
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
+            <img src="/images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
         <div className="media-content">
-          <p className="title is-8">{title}</p>
+          <p className="title is-8">{info.title}</p>
         </div>
       </div>
 
       <div className="content">
-        {description}
+        {info.description}
         <br />
-        <a href={imdbUrl}>IMDB</a>
+        <a href={info.imdbUrl}>IMDB</a>
       </div>
     </div>
   </div>
