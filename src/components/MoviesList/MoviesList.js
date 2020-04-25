66fec47450d969bb04897cb416001a9292204527
Kostya Peniyavskiy <kornelius9898@gmail.com>
2020-04-25 20:07:08 +0300

    changed key prop for MovieCard component

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -6,9 +6,11 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MoviesList = ({ movies }) => (
   <>
     <div className="movies">
-      { movies.map(movie => (
-        <MovieCard key={movies.imdbId} {...movie} />
-      )) }
+      {
+        movies.map(movie => (
+          <MovieCard key={movie.imdbId} {...movie} />
+        ))
+      }
     </div>
   </>
 );
