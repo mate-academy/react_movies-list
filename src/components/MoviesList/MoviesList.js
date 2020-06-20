b97bbb550b568c2ed3a388caa55f3f9db0ee4202
solaryasha <solaryasha@gmail.com>
2020-06-21 00:45:22 +0300

    My attempt to refactor movie list

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = props => (
+  <div className="page">
+    <div className="page-content">
+      <div className="movies">
+        {props.movies.map(movie => (
+          <MovieCard {...movie} />))
+        }
+      </div>
+    </div>
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </div>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf.isRequired,
+};
