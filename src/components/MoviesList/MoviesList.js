16c4ca68c126c83da6b30cf17dc541daf8f4e0ae
Glib Rulev <velzepooz@gmail.com>
2020-06-22 14:55:39 +0300

    Fixed

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -6,9 +6,7 @@ import MovieCard from '../MovieCard/MovieCard';
 const MoviesList = ({ movies }) => (
 
   <div className="movies">
-    {
-      movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)
-    }
+    {movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
   </div>
 );
 
