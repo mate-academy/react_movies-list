e8307f85317d65be9d532b461f73823a031b01d8
kvalenty <krushunin89@gmail.com>
2020-06-23 16:46:11 +0300

    change shape to object

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-export const ShapeMovieCard = PropTypes.shape({
+export const ShapeMovieCard = PropTypes.object({
   imgUrl: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
