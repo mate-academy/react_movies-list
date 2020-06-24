02fee7cda9e3112e92bc59418f7b46a5edf4b35f
Liliia <chuiko1112@gmail.com>
2020-06-24 12:33:50 +0300

    visual

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import MoviesList from './components/MoviesList/MoviesList';
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <div className="movies">
-        <MoviesList movies={moviesFromServer} />
-      </div>
+      <MoviesList movies={moviesFromServer} />
+
     </div>
 
     <div className="sidebar">
