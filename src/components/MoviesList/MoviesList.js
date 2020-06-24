a73ddbd106f6cb6eeca808b94848f24d26a54096
DTR <t.terry.rice@gmail.com>
2020-06-24 13:39:51 +0300

    final 1.0.3

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
 import { MovieCard } from '../MovieCard/MovieCard';
 
 export const MoviesList = ({ movies }) => (
-  movies.map(specificFilm => (
-    <div className="card" key={specificFilm.imdbId}>
-      <MovieCard {...specificFilm} />
+  movies.map(movie => (
+    <div className="card" key={movie.imdbId}>
+      <MovieCard {...movie} />
     </div>
   ))
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    imgUrl: PropTypes.string.isRequired,
+    imdbUrl: PropTypes.string.isRequired,
+    imdbId: PropTypes.string.isRequired,
+  })).isRequired,
+};
