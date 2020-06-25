3b6ccaf0bdcda9773ce372ab1232b2878d090ef2
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-25 23:05:28 +0300

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
+import { cardsProp } from '../../PropTypes';
 
-export const MoviesList = () => (
-  <>Put the list here</>
-);
+export const MoviesList = (props) => {
+  const { movies } = props;
+
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+          imdbId={movie.imdbId}
+        />
+      ))}
+    </div>
+  );
+};
+
+MoviesList.propTypes = cardsProp.isRequired;
