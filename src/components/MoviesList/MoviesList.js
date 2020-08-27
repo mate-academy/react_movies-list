ad13028004122f64658af137a8d51d7cc11946cd
ArtemiiKab <artemiikab@gmail.com>
2020-08-27 14:10:33 +0300

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = props => (
+  <>
+    <div className="movies">
+      {Object.keys(props)
+        .map(movie => (
+          <MovieCard
+            key={props[movie].imdbId}
+            {...props[movie]}
+          />
+        ))
+      }
+    </div>
+  </>
 );
