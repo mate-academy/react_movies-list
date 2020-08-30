97ba6c2ba3c3f003a9c608152037771c88aec741
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-30 16:39:52 +0300

    +

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ list }) => (
+  <div className="movies">
+    {list.map(item => (
+      <MovieCard
+        {...item}
+        key={item.imdbId}
+      />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      imgUrl: PropTypes.string.isRequired,
+      imdbUrl: PropTypes.string.isRequired,
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  list: [],
+};
