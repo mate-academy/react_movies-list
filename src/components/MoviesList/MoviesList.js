33d79c47ba7b5789d0c20e732a34ac9e72950e2f
Andrii Suboch <andrii.suboch@gmail.com>
2020-08-31 16:28:34 +0300

    Solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './MoviesList.scss';
+import movies from '../../api/movies.json';
+import { MovieCard } from '../MovieCard';
 
 export const MoviesList = () => (
-  <>Put the list here</>
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        {...movie}
+      />
+    ))}
+  </div>
 );
