96a729d960279ce210fd04a829b5572d80bd7337
Pavlo <pavlo.com.5@gmail.com>
2020-09-01 17:31:07 +0300

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+
+export const MoviesList = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+
+        <div className="card" key={movie.imdbId}>
+          <MovieCard {...movie} />
+        </div>
+      ))}
+    </div>
+  </>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  movies: [],
+};
