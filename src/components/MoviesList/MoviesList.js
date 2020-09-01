13a5bffb326ea7b6c42b7d18e17d68b6efa76cd8
nznmvs <nznmvs@gmail.com>
2020-09-01 10:56:26 +0300

    SolutionFix2

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -16,10 +16,6 @@ export const MoviesList = ({ movies }) => (
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      imgUrl: PropTypes.string.isRequired,
-      imdbUrl: PropTypes.string.isRequired,
       imdbId: PropTypes.string.isRequired,
     }),
   ),
