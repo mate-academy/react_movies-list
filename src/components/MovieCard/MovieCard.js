2123e54379ef2f704956d38c74770fa63454ccc1
Vasyl_Zarichnyi <vasylzarichnyi@gmail.com>
2020-09-01 15:16:54 +0300

    solution

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ imgUrl, title, description, imdbUrl }) => (
+
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img src={imgUrl} alt="Film logo" />
+      </figure>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl}>IMDB</a>
+        </div>
+      </div>
+    </div>
+  </div>
 );
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  imdbUrl: PropTypes.string.isRequired,
+  description: PropTypes.string,
+};
+
+MovieCard.defaultProps = {
+  description: 'description',
+};
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ films }) => (
+  <div className="movies">
+    {films.map(film => (
+      <MovieCard {...film} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  films: [],
+};
