8c8b9918480f78b6f9e6139880e900b1f36ba013
Vlad Savustianenko <vladsavustianenko@gmail.com>
2020-09-02 16:57:12 +0300

    Solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+import MovieCard from '../MovieCard/MovieCard';
+
+import data from '../../api/movies.json';
+
+const MoviesList = () => (
+  data.map(movie => (
+    <MovieCard
+      key={movie.imdbId}
+      title={movie.title}
+      description={movie.description}
+      imgUrl={movie.imgUrl}
+      imdbUrl={movie.imdbUrl}
+      imdbId={movie.imdbId}
+    />
+  ))
 );
+
+export default MoviesList;
