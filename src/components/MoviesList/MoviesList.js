8964e7146444e1f02210b77c095fe3782243cbeb
Alona <aionovaionova@gmail.com>
2020-10-19 14:08:04 +0300

    done

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ list }) => (
+  <div className="movies">
+    {list.map(movie => (
+      <MovieCard {... movie} key="movie.imdbId" />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape = {
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      imgUrl: PropTypes.string.isRequired,
+      imdbUrl: PropTypes.string.isRequired,
+    },
+  ).isRequired,
+};
