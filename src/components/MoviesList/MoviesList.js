e0ef5f15d33835271f4210c0ed71ae8aeea49b88
Alina <alina90565@gmail.com>
2020-10-19 22:57:52 +0300

    done movies film task

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ films }) => (
+  <ul className="movies">
+    {films.map(film => (
+      <li className="card" key={film.imdbId}>
+        <MovieCard {...film} />
+      </li>
+    ))}
+  </ul>
 );
+
+MoviesList.propTypes = {
+  films: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+      imgUrl: PropTypes.string,
+      imdbUrl: PropTypes.string,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  films: {},
+};
