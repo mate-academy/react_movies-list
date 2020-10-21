5655809634469ffec4f05299f5cc011d670f5415
Artem Lavreniuk <artemlavrenyuk@gmail.com>
2020-10-21 17:28:57 +0300

    fixed propType issue and key of movieCard component

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -5,17 +5,17 @@ import { MovieCard } from '../MovieCard';
 
 export const MoviesList = ({ movies }) => (
   <div className="movies">
-    {movies.map(movie => <MovieCard {...movie} key="movie.imdbId" />)}
+    {movies.map(movie => <MovieCard {...movie} key={movie.imdbId} />)}
   </div>
 );
 
 MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
-    PropTypes.shape = {
+    PropTypes.shape({
       title: PropTypes.string.isRequired,
       description: PropTypes.string,
       imgUrl: PropTypes.string.isRequired,
       imdbUrl: PropTypes.string.isRequired,
-    },
+    }),
   ).isRequired,
 };
