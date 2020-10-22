cb575aacc1611019332392717e7d552cf7912dae
Evgenii <voznukzhenia@gmail.com>
2020-10-22 15:20:47 +0300

    movies

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
+import { MovieCardShape } from '../shape/MovieCardShape';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ lists }) => (
+  <div className="movies">
+    {lists.map(movie => (
+      <MovieCard {...movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  lists: PropTypes.arrayOf(MovieCardShape).isRequired,
+};
