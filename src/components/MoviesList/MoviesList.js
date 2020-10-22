edc3b68f310047f512b70fa89559fa0f61688219
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-22 10:01:33 +0300

    movies list

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { MoviesListShape } from './MoviesListShape';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" key={movie.imdbId}>
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = MoviesListShape;
---
+++ src/components/MoviesList/MoviesListShape.js
@@ -0,0 +1,5 @@
+import PropTypes from 'prop-types';
+
+export const MoviesListShape = PropTypes.shape({
+  movies: PropTypes.object.isRequired,
+});
