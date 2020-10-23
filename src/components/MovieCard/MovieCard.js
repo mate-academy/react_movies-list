64f769deaa7a66972531eb9a9709160671487fec
Evgeniy <more.inspirationn@gmail.com>
2020-10-23 10:08:54 +0300

    add src imbd

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -22,7 +22,7 @@ export const MovieCard = ({ title, description, imgUrl, imdbUrl, imdbId }) => (
         <div className="media">
           <div className="media-left">
             <figure className="image is-48x48">
-              <img src="/images/imdb-logo.jpeg" alt="imdb" />
+              <img src="./images/imdb-logo.jpeg" alt="imdb" />
             </figure>
           </div>
 
