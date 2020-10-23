0474d49c165568cb906bfb23eb405c420502917d
Olena kuzii <okuziy@gmail.com>
2020-10-23 09:54:02 +0300

    Solution

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import './MovieCard.scss';
+import propTypes from 'prop-types';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ title, description, imgUrl, imdbUrl }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="/images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+      <div className="content">
+        {description}
+        <br />
+        <a href={imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
+
+MovieCard.defaultProps = {
+  description: '',
+};
+
+MovieCard.propTypes = {
+  title: propTypes.string.isRequired,
+  description: propTypes.string,
+  imgUrl: propTypes.string.isRequired,
+  imdbUrl: propTypes.string.isRequired,
+};
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './MoviesList.scss';
+import propTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard {...movie} key={movie.imbdId} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  movies: propTypes.arrayOf(
+    propTypes.shape({
+      imbdId: propTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
