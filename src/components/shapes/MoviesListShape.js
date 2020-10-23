1280fcaf66aa4199c6e337f326fb440fa7122cf5
KirillLutsenko <mnrovski68@gmail.com>
2020-10-23 11:55:43 +0300

    fixes MoviesListShape propTypes

--- src/components/shapes/MoviesListShape.js
+++ src/components/shapes/MoviesListShape.js
@@ -2,5 +2,5 @@ import PropTypes from 'prop-types';
 import { MovieCardShape } from './MovieCardShape';
 
 export const MoviesListShape = {
-  movies: PropTypes.arrayOf(MovieCardShape.object).isRequired,
+  movies: PropTypes.arrayOf(MovieCardShape).isRequired,
 };
