7ee8e05dc68d86f7c9bdb742f9ff9a08b54eae71
KirillLutsenko <mnrovski68@gmail.com>
2020-10-23 10:07:43 +0300

    taking out the sidebar as a separate component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
+import { Sidebar } from './components/Sidebar';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
       <MoviesList movies={moviesFromServer} />
     </div>
-
-    <div className="sidebar">
-      Sidebar will be here
-    </div>
+    <Sidebar />
   </div>
 );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Sidebar.css';
+
+export const Sidebar = () => (
+  <div className="sidebar">
+    Sidebar will be here
+  </div>
+);
