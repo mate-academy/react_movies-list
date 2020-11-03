1a738cba5509682af03084f5af26d79b0ce1d854
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-03 11:52:43 +0200

    done

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,32 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable arrow-parens */
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/index';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <ul className="movies">
+    {movies.map((movie) => (
+      <li key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
+    ))}
+  </ul>
 );
+
+MoviesList.defaultProps = {
+  movies: [],
+};
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      imgUrl: PropTypes.string.isRequired,
+      imdbUrl: PropTypes.string.isRequired,
+      imdbId: PropTypes.string.isRequired,
+    })
+  ),
+};
