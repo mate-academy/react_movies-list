76848a37ebcb5bc2b177d3bec54bd4982275b812
RioDron <riopositivniy@gmail.com>
2021-01-11 23:30:44 +0200

    revisions for img

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -17,7 +17,7 @@ export const MovieCard = ({ title, imgUrl, imdbUrl, description }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            <img src="./images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
