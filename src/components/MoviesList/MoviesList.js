bd6f171056162bf70700f619c81fc7efdd08450f
PaulT <ptimofejev@gmail.com>
2021-01-12 12:49:02 +0200

    Movie list solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
+import { MovieCardType } from '../../types';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ source }) => (
+  <div className="movies">
+    {source.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
+
+MoviesList.defaultProps = {
+  source: '',
+};
+
+MoviesList.propTypes = {
+  source: PropTypes.arrayOf(MovieCardType),
+};
