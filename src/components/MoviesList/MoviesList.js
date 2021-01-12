5b2083aa4adf2bef9de472e3118a1db670058c74
Lora Levitska <larakad94@gmail.com>
2021-01-12 13:35:32 +0200

    split movieList and movieCard

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { TypeofValues } from '../../types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <>
+    {movies.map(movie => (
+      <div className="card" key={movie.imdbId}>
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(TypeofValues).isRequired,
+};
