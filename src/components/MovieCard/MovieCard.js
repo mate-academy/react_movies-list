cecd9ede8b70f54920f4cc764425dff78ef3c20e
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-13 15:19:37 +0200

    Added fixes

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -16,7 +16,7 @@ const MovieCard = ({ title, description, imdbUrl, imgUrl }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            <img src="./images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
