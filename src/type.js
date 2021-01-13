10fe23d9b7c6741255ee69ee8df110dfd020cc56
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-13 12:29:02 +0200

    Fixed type

--- src/type.js
+++ src/type.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 
 export const TypeMovie = PropTypes.shape({
   title: PropTypes.string.isRequired,
+  description: PropTypes.string,
   imgUrl: PropTypes.string.isRequired,
   imdbUrl: PropTypes.string.isRequired,
 });
