3338f3f718d3d02ac31548105886a4ab3bb9448e
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-13 18:47:23 +0200

    made task

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import './MoviesList.scss';
+import PropTypes from 'prop-types';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { TypeFilm } from '../../type';
+
+export const MoviesList = ({ films }) => (
+  <div className="movies">
+    {films.map(film => (<MovieCard key={film.imdbId} {...film} />))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  films: PropTypes.arrayOf(TypeFilm).isRequired,
+};
