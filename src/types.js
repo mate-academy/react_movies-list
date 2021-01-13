386b4120d7e63c77334162ffd00eb2a6999a28b4
Dima Bond <neverpooh@gmail.com>
2021-01-13 15:58:26 +0200

    all files

---
+++ src/types.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export const TypeCard = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imgUrl: PropTypes.string.isRequired,
+  imdbUrl: PropTypes.string.isRequired,
+  imdbId: PropTypes.string.isRequired,
+});
