cbed6077c4281018d933deff384673fd68677f4e
Layrovell <marina95prodan@gmail.com>
2021-01-13 12:32:32 +0200

    movie list

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
-);
+export const MovieList = ({ movies }) => {
+  const MoviesArray = movies.map(el => (
+    <div className="movies" key={el.imdbId}>
+      <MovieCard
+        title={el.title}
+        description={el.description}
+        imgUrl={el.imgUrl}
+        imdbUrl={el.imdbUrl}
+        imdbId={el.imdbId}
+      />
+    </div>
+  ));
+
+  return (
+    MoviesArray
+  );
+};
