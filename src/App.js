fd14a578893f949567a4b09b080d559fccd27550
Dima Semenov <semenovdima998@gmail.com>
2021-01-14 14:48:45 +0200

    Fixed problems

--- src/App.js
+++ src/App.js
@@ -4,5 +4,17 @@ import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList/MoviesList';
 
 export const App = () => (
-  <MoviesList movies={moviesFromServer} />
+  <div className="page">
+    <div className="page-content">
+      <div className="movies">
+
+        <MoviesList movies={moviesFromServer} />
+
+      </div>
+    </div>
+
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -2,33 +2,16 @@ import React from 'react';
 import './MoviesList.scss';
 import PropTypes from 'prop-types';
 import { MovieCard } from '../MovieCard/MovieCard';
+import { MovieType } from '../Type';
 
 export const MoviesList = ({ movies }) => (
-  <div className="page">
-    <div className="page-content">
-      <div className="movies">
-
-        {movies.map(item => (
-          <MovieCard {...item} key={item.imdbId} />
-        ))}
-
-      </div>
-    </div>
-
-    <div className="sidebar">
-      Sidebar will be here
-    </div>
-  </div>
+  <>
+    {movies.map(item => (
+      <MovieCard {...item} key={item.imdbId} />
+    ))}
+  </>
 );
 
-const types = PropTypes.shape({
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  imgUrl: PropTypes.string.isRequired,
-  imdbUrl: PropTypes.string.isRequired,
-  imdbId: PropTypes.string.isRequired,
-});
-
 MoviesList.propTypes = {
-  movies: PropTypes.arrayOf(types).isRequired,
+  movies: PropTypes.arrayOf(MovieType).isRequired,
 };
