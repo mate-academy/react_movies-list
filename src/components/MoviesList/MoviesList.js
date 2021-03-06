876458b385f43cec55ce2056dc383e4b5e3f054a
antoniakstepan <antoniakstepan1@gmail.com>
2021-03-06 10:18:46 +0200

    first commit

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
-);
+export const MoviesList = ({ movies }) => {
+  const movieList = movies.map(
+    movie => <MovieCard key={movie.imbdId} {...movie} />,
+  );
+
+  return (
+    <div className="movies">
+      {movieList}
+    </div>
+  );
+};
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  movies: [],
+};
