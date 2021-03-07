0f55ee74d04b634fc484b8702b63c66001eabb17
Taras <seman6745@gmail.com>
2021-03-07 15:47:51 +0200

    first commit

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
+import './MoviesList.scss';
+
+export const MoviesList = ({ listOfFilms }) => (
+  <>
+    <div className="page-content">
+      <ul className="movies">
+        {listOfFilms.map(film => (
+          <li className="card" key={listOfFilms.imdbId}>
+            <MovieCard {...film} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  </>
+);
+
+MoviesList.propTypes = {
+  listOfFilms: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  listOfFilms: [],
+};
