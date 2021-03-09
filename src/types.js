86e4f0833bdd957a9291f98772bd9381e32c9f2d
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-09 20:15:28 +0200

    solution was fixed

---
+++ src/types.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+
+export const MovieType = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  imdbPath: PropTypes.string.isRequired,
+});
