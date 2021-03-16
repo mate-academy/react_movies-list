5e95b2ab0f81fc9ac3ea8f8ea5b9bb1bf5eaad04
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-16 19:03:46 +0200

    first commit

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ moviesList }) => (
+  <div className="movies">
+    {
+      moviesList.map(film => (
+        <div className="card" key={film.imdbId}>
+          <MovieCard {...film} />
+        </div>
+      ))
+    }
+  </div>
 );
+
+MoviesList.propTypes = {
+  moviesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+MoviesList.defaultProps = {
+  moviesList: [],
+};
