ce48cbecd8cfe481bdd371508526f4233143c07b
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-16 21:14:03 +0200

    solution

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -5,13 +5,11 @@ import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
 export const MoviesList = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard {...movie} key={movie.imbdId} />
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard {...movie} key={movie.imbdId} />
+    ))}
+  </div>
 );
 
 MoviesList.propTypes = {
