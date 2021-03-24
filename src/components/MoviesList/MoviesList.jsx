d8d03d25c9a37f0e7022798279141bb4ec384dfd
Smihotur Roman <smikhotur@gmail.com>
2021-03-24 23:18:18 +0200

    Solution

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
+
+export const MoviesList = ({ movies }) => (
+  <>
+    <div className="page-content">
+      <div className="movies">
+        {movies.map(film => (
+          <div key={film.imdbId} className="card">
+            <MovieCard {...film} />
+          </div>
+        ))}
+        ;
+      </div>
+    </div>
+
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </>
+);
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
