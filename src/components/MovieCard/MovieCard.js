20d512b0a4a9b1e16e935fe6a9234d9f09ab1dba
Ihor Drul <druleger@gmail.com>
2021-03-30 14:44:01 +0300

    Solution

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({
+  title,
+  description,
+  imgUrl,
+  imdbUrl,
+}) => (
+  <>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={imgUrl}
+            alt={`${title} film poster`}
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl} target="_blanc">IMDB</a>
+        </div>
+      </div>
+    </div>
+  </>
 );
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imgUrl: PropTypes.string.isRequired,
+  imdbUrl: PropTypes.string.isRequired,
+};
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './sidebar.scss';
+
+export const Sidebar = () => (
+  <div className="sidebar">
+    Sidebar will be here
+  </div>
+);
