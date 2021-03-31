c2410d04c0a05af188658676d3e4015d50094c4b
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-31 19:47:25 +0300

    second solution

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import moviesFromServer from './api/movies.json';
+import { MoviesList } from './components/MoviesList';
+
+export const App = () => (
+  <div className="page">
+    <MoviesList movies={moviesFromServer} />
+
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </div>
+);
