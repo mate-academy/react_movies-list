72f345aa3ecbe3677835e88439d971542f334d81
Ihor Karpyn <igor82200@gmail.com>
2021-05-17 18:41:18 +0300

    split card render

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
-);
+export function MoviesList({ movies }) {
+  return (
+    <div className="movies">
+      {movies.map(movie => <MovieCard {...movie} key={movie.imdbId} />)}
+    </div>
+  );
+}
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
