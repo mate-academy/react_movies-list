09c62996bcadc2e1f29f57bf9d5ede3fa8593ec7
Taras Kozak <kozaktarasofficial@gmail.com>
2021-05-18 15:55:11 +0300

    v1.00

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import propTypes from 'prop-types';
+
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div className="card" key={movie.imdbId}>
+          <MovieCard {...movie} />
+        </div>
+      ))}
+    </div>
+  </>
 );
+
+MoviesList.propTypes = {
+  movies: propTypes.arrayOf(propTypes.shape({
+    imdbId: propTypes.string,
+  })).isRequired,
+};
