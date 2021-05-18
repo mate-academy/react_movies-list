c7a28b847d1b7ea459d4bffe11028b17e9db1074
Dima Batiuk <batiuk20@gmail.com>
2021-05-18 18:09:18 +0300

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MoviesList.scss';
 import PropTypes from 'prop-types';
-import { MovieCard } from '../MovieCard/MovieCard';
+import { MovieCard } from '../MovieCard/index';
 
 export const MoviesList = ({ movies }) => (
   <div className="movies">
