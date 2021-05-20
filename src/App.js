53b4b974e95ee6aa58a02ac16200eca0e1de4c3a
Vasyl Habalevych <vasylhab@gmail.com>
2021-05-20 10:48:43 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
-      </div>
-      <div className="sidebar">
-        Sidebar will be here
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MoviesList movies={moviesFromServer} />
     </div>
-  </>
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </div>
 );
