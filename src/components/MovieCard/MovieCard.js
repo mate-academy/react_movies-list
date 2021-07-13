02c3ab0a73c828af6fb6d1d137014f62ff28f5b9
Oleksii Lymonov <alex25_92@ukr.net>
2021-07-13 16:33:19 +0300

    first_try

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ film }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          // eslint-disable-next-line max-len
+          src={film.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{film.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {film.description}
+        <br />
+        <a href={film.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
+
+MovieCard.defaultProps = { film: PropTypes.shape({
+  description: '',
+}) };
+
+MovieCard.propTypes = { film: PropTypes.shape({
+  imgUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imdbUrl: PropTypes.string.isRequired,
+}) };
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ moviesFromServer }) => (
+  <div className="movies">
+    {moviesFromServer.map(film => (
+      <MovieCard film={film} key={film.imdbId} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  moviesFromServer: PropTypes.arrayOf(PropTypes.shape({
+    imdbId: PropTypes.string.isRequired,
+  })).isRequired,
+};
