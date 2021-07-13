2356dd115e75d855a4fba11f3149bbf9122fa4ae
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-13 16:21:45 +0300

    Splited movies list components

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
+        {...movie}
+      />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    imgUrl: PropTypes.string,
+    imdbId: PropTypes.string,
+    imdbUrl: PropTypes.string,
+  })).isRequired,
+};
