62556ea0ed6e4d8c053b807986d32af8589593e3
David Dohru <tronsmartmy@gmail.com>
2021-07-15 00:22:38 +0300

    done

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Test } from '../MovieCard/Test';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+const MovieList = ({ moviesFromServer }) => (
+  <div className="page-content">
+    <div className="movies">
+      { moviesFromServer.map(x => <MovieCard key={x.title} {...x} />) }
+    </div>
+  </div>
 );
+
+MovieList.propTypes = {
+  moviesFromServer: PropTypes.arrayOf(
+    PropTypes.shape(Test),
+  ).isRequired,
+};
+
+export default MovieList;
