3f4f3fe8ef19f18433dc9c5d4fbf57f7ba810b68
Stas Pechenyi <spechenuy@gmail.com>
2021-07-15 15:29:06 +0300

    Homework was done

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './MovieCard.scss';
+
+export const MovieCard = ({
+  imgUrl,
+  title,
+  description,
+  imdbUrl,
+}) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          // eslint-disable-next-line max-len
+          src={imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {description}
+        <br />
+        <a href={imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
+);
+
+MovieCard.defaultProps = {
+  description: "Sorry, we can't find the description(",
+};
+
+MovieCard.propTypes = {
+  title: propTypes.string.isRequired,
+  description: propTypes.string,
+  imgUrl: propTypes.string.isRequired,
+  imdbUrl: propTypes.string.isRequired,
+};
---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
+import './MoviesList.scss';
+
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard {...movie} key={movie.imdbId} />
+    ))}
+  </div>
+);
+
+MoviesList.propTypes = {
+  movies: propTypes.arrayOf(
+    propTypes.shape({
+      imdbId: propTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
