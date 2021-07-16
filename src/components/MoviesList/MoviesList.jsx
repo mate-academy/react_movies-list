b076ac2ef5dc39021054bcefebf22188e7eede65
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-07-16 16:44:32 +0300

    movies react

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
+
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" key={movie.imdId}>
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
+);
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
