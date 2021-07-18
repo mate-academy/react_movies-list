f5a8ff5bba67cae61f0ce1e59ed116d9f7c4cc6d
Artem <temaqwerty15@gmail.com>
2021-07-18 19:08:01 +0300

    fin

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard';
+
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div key={movie.imdbId} className="card">
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
+);
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
