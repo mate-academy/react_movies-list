7767585d855ff10bcb5b2f6c062c7333fe0d19ff
Andrii Balitskyi <10balian10@gmail.com>
2021-07-19 13:18:08 +0200

    split into components

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import moviesFromServer from '../../api/movies.json';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ imdbId }) => (
+  <div className="movies">
+    {moviesFromServer.map(movie => <MovieCard {...movie} key={imdbId} />)}
+  </div>
 );
+
+MoviesList.propTypes = {
+  imdbId: PropTypes.string.isRequired,
+};
