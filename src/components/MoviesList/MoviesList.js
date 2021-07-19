fa2418050db8e87aece70922ae712b7ef7b0fd5a
sergei-trofimov <feast.st@gmail.com>
2021-07-19 10:50:40 +0300

    Fixed propTypes and code style

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -5,11 +5,13 @@ import { MovieCard } from '../MovieCard/MovieCard';
 
 export const MoviesList = ({ movies }) => (
   <ul className="movies">
-    {movies.map(movie => (
-      <li className="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
-      </li>
-    ))}
+    {
+      movies.map(movie => (
+        <li className="card" key={movie.imdbId}>
+          <MovieCard {...movie} />
+        </li>
+      ))
+    }
   </ul>
 );
 
@@ -17,6 +19,10 @@ MoviesList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.shape({
       imdbId: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      imgUrl: PropTypes.string.isRequired,
+      imdbUrl: PropTypes.string.isRequired,
     }),
   ),
 };
