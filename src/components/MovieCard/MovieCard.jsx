151226caaabbfa16004c79aefa1d869e77fe6649
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-07-19 12:42:15 +0300

    added demo link

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './MovieCard.scss';
+import PropTypes from 'prop-types';
+
+export const MovieCard = ({ movie }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={movie.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{movie.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {movie.description}
+        <br />
+        <a href={movie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
+);
+
+MovieCard.defaultProps = { movie: PropTypes.shape({
+  description: '',
+}) };
+
+export const movieCardTypes = PropTypes.shape({
+  imgUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imdbUrl: PropTypes.string.isRequired,
+  imdbId: PropTypes.string.isRequired,
+});
+
+MovieCard.propTypes = { movie: movieCardTypes };
