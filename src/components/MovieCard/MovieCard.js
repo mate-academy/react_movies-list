a9e9761fe6780b41be33867fd1ab34d4cb6c2bf8
Sergiy Fartushniak <fartux97@gmail.com>
2021-07-19 13:50:12 +0300

    app was splited into two components

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,6 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './MovieCard.scss';
 
-export const MovieCard = () => (
-  <>Put the card here</>
+export const MovieCard = ({ movie }) => (
+  <>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={movie.imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">
+              {movie.title}
+            </p>
+          </div>
+        </div>
+
+        <div className="content">
+          {movie.description}
+          <br />
+          <a href={movie.imdbUrl}>IMDB</a>
+        </div>
+      </div>
+    </div>
+  </>
 );
+
+export const movieProps = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imgUrl: PropTypes.string.isRequired,
+  imdbUrl: PropTypes.string.isRequired,
+  imdbId: PropTypes.string.isRequired,
+});
+
+MovieCard.propTypes = {
+  movie: movieProps,
+};
+
+MovieCard.defaultProps = {
+  movie: PropTypes.shape({
+    description: '',
+  }),
+};
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard, movieProps } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+
+    </div>
+  </>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(movieProps).isRequired,
+};
