a5b1f38e13af1343df869a99e3098874930d12fa
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-07-19 09:43:41 +0300

    movies update-01

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+
+export const MoviesList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(card => (
+      <div key={card.imdbId}>
+        <MovieCard
+          title={card.title}
+          description={card.description}
+          imgUrl={card.imgUrl}
+          imdbUrl={card.imdbUrl}
+          imdbId={card.imdbId}
+        />
+      </div>
+    ))}
+  </div>
+);
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.object.isRequired,
+  ).isRequired,
+};
