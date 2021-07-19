c678b5c9ddbdac7acd48c9fd0862ac3bdb06d2d1
NazarYanchyshyn <yanchishin12@ukr.net>
2021-07-19 15:57:09 +0300

    Solution

---
+++ src/MoviePropTypes.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+
+export const MoviePropTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imgUrl: PropTypes.string.isRequired,
+  imdbUrl: PropTypes.string.isRequired,
+};
