6c8c27b76a7d5d5bd3184cc7afaae410af3b9791
Liudmyla Yevchuck <lol22lil22@gmail.com>
2021-07-19 17:14:06 +0300

    Movies list

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movies }) => (
+  <>
+    {
+      movies.map(
+        movie => (
+          <React.Fragment key={movie.imdbId}>
+            <MovieCard {...movie} />
+          </React.Fragment>
+        ),
+      )
+    }
+  </>
 );
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      imdbId: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
