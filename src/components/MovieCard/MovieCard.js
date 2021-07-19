80ea0951eb3ea3d1a7d8b2c20d733b5300e7102d
Vtlk Mrtnk <vitaliymartiniuk16@gmail.com>
2021-07-19 12:34:44 +0300

    movies app

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './MovieCard.scss';
+import { shapeOfCard } from '../../cardShape';
 
-const MovieCard = () => (
-  <>
-    <div>Test</div>
-  </>
+export const MovieCard = ({ movieItem }) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          // eslint-disable-next-line max-len
+          src={movieItem.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{ movieItem.title }</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {movieItem.description || 'No description'}
+        <br />
+        <a href={movieItem.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
 
-export default MovieCard;
+MovieCard.propTypes = {
+  movieItem: shapeOfCard.isRequired,
+};
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './MoviesList.scss';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
+import { shapeOfCard } from '../../cardShape';
 
-export const MoviesList = () => (
-  <>Put the list here</>
+export const MoviesList = ({ movieList }) => (
+  <div className="movies">
+    {movieList.map(card => (
+      <MovieCard key={card.title} movieItem={card} />
+    ))}
+  </div>
 );
+
+MoviesList.propTypes = {
+  movieList: PropTypes.arrayOf(shapeOfCard).isRequired,
+};
