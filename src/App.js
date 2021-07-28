5c07942155af91dcca700537aec47442dbbcaac4
tkachukione <tkachukione@gmail.com>
2021-07-29 01:37:49 +0300

    first solution

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MoviesList movies={moviesFromServer} />
-    </div>
+export class App extends React.Component {
+  state = {
+    query: '',
+  }
 
-    <div className="sidebar">
-      Sidebar will be here
-    </div>
-  </div>
-);
+  render() {
+    const { query } = this.state;
+    const filteredMovie = moviesFromServer.filter(movie => (
+      movie.description.toLowerCase().includes(query.toLowerCase())
+      || movie.title.toLowerCase().includes(query.toLowerCase())
+    ));
+
+    return (
+      <>
+        <input
+          className="input"
+          type="text"
+          placeholder="write title or description of the movie"
+          onChange={(event) => {
+            this.setState({
+              query: event.target.value,
+            });
+          }}
+        />
+        <div className="page">
+          <div className="page-content">
+            <MoviesList movies={filteredMovie} />
+          </div>
+          <div className="sidebar">
+            Sidebar will be here
+          </div>
+        </div>
+      </>
+    );
+  }
+}
