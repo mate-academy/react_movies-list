40360d4e68079f7524f9b6aa8be58048405a071b
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-08-30 11:50:09 +0300

    Added props interface for MovieCard.tsx

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,15 +3,15 @@ import './MoviesList.scss';
 import { Movie } from '../../types/Movie';
 import { MovieCard } from '../MovieCard';
 
-type Props = {
-  movies?: Movie[],
-};
+interface Props {
+  movies: Movie[],
+}
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
-    {movies?.map(movie => (
+    {movies.map(movie => (
       <div className="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
+        <MovieCard movie={movie} />
       </div>
     ))}
   </div>
