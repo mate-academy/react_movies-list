9b0c6fb577a2d9e67fec838ed6f34fcbd70a22d3
lera-yaskevych <redrum.back@gmail.com>
2021-08-30 14:10:52 +0300

    code style edit

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -9,42 +9,46 @@ type Props = {
   imdbUrl: string;
 };
 
-export const MovieCard: React.FC<Props> = ({
-  title,
-  description,
-  imgUrl,
-  imdbUrl,
-}) => (
-  <>
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
+export const MovieCard: React.FC<Props> = (props) => {
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+  } = props;
 
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img src="images/imdb-logo.jpeg" alt="imdb" />
-            </figure>
-          </div>
-
-          <div className="media-content">
-            <p className="title is-8">{title}</p>
-          </div>
+  return (
+    <>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={imgUrl}
+              alt="Film logo"
+            />
+          </figure>
         </div>
 
-        <div className="content">
-          {description}
-          <br />
-          <a href={imdbUrl}>IMDB</a>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8">{title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {description}
+            <br />
+            <a href={imdbUrl}>IMDB</a>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
