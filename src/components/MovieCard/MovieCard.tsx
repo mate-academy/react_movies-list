477eac0b3fe8607e600f2a974fdf9dfa353011bc
Polina Razumna <poliandiy@gmail.com>
2021-08-31 19:25:54 +0300

    Added spread

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -9,40 +9,49 @@ type Props = {
   imdbUrl: string,
 };
 
-export const MovieCard: React.FC<Props> = (props) => (
-  <>
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={props.imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
+export const MovieCard: React.FC<Props> = (props) => {
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+  } = props;
 
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img src="images/imdb-logo.jpeg" alt="imdb" />
-            </figure>
-          </div>
-
-          <div className="media-content">
-            <p className="title is-8">{props.title}</p>
-          </div>
+  return (
+    <>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={imgUrl}
+              alt="Film logo"
+            />
+          </figure>
         </div>
 
-        <div className="content">
-          {props.description ? props.description : ''}
-          <br />
-          <a href={props.imdbUrl}>IMDB</a>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8">{title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {description}
+            <br />
+            <a href={imdbUrl}>IMDB</a>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 MovieCard.defaultProps = {
   description: '',
