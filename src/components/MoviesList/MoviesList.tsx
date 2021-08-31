96cfcba50d8525f7f515f7692a3d2cc1355489ce
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-31 15:34:47 +0300

    add MovieCard and MoviesList components

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    <ul>
+      {movies.map(movie => (
+        <li key={movie.imdbId}>
+          <MovieCard {...movie} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
