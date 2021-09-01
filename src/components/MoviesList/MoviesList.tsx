7940ff4e89b5aafef1a9614f8cb7df97adfa9f3f
Dima Tkachenko <DimaTkachenko3@gmail.com>
2021-09-01 13:36:21 +0300

    fixes was added

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,23 +1,16 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
-
-interface Movie {
-  title: string,
-  description: string,
-  imgUrl: string,
-  imdbUrl: string,
-  imdbId: string,
-}
+import { Movie } from '../../types/Movie';
 
 type Props = {
   movies: Movie[];
 };
 
-export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <div className="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
+        <MovieCard movie={movie} />
       </div>
     ))}
   </div>
