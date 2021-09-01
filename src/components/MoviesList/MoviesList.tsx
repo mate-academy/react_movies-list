4f84fcac8e89a3e41f70971f2685d449ec955627
Roman Fedusevych <romanfyks@gmail.com>
2021-09-01 21:58:50 +0300

    use movie interface, fix moviesList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -12,7 +12,7 @@ export const MoviesList: React.FC<Props> = ({ moviesList }) => (
     {moviesList.map(movie => (
       <MovieCard
         key={movie.imdbId}
-        {...movie}
+        movie={movie}
       />
     ))}
   </div>
