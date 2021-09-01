eecff8d60610277b60cf2d3e0da577306836c305
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-09-01 12:14:24 +0300

    add Movie type

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import './MoviesList.scss';
 
 import { MovieCard } from '../MovieCard';
-
-interface Movie {
-  title: string,
-  description: string,
-  imgUrl: string,
-  imdbUrl: string,
-  imdbId: string,
-}
+import { Movie } from '../../types/Movie';
 
 type Props = {
   movies: Movie[];
 };
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <ul className="moviesList">
-    {movies.map(movie => (
-      <li key={movie.imdbId}>
-        <MovieCard {...movie} />
-      </li>
-    ))}
-  </ul>
-);
+export const MoviesList: React.FC<Props> = (props) => {
+  const { movies = [] } = props;
+
+  return (
+    <ul className="moviesList">
+      {movies.map(movie => (
+        <li key={movie.imdbId}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  );
+};
