4fb55d966f1f2661a2b65dab11a410c3c96b2f58
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-09-02 23:38:09 +0300

    MoviesList done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <ul className="movies">
+    {movies.map(movie => (
+      <li className="card">
+        <MovieCard key={movie.imdbId} movie={movie} />
+      </li>
+    ))}
+  </ul>
 );
