34b51bba4fc61564693439f6c964ebe559fcf24a
unknown <hado.roman.ihorovych@gmail.com>
2021-09-02 16:23:58 +0300

    added Props and some fixes

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
-import { Movie } from '../types/Movie';
 import { MovieCard } from '../MovieCard';
 
 type Props = {
   movies: Movie[];
 };
 
-export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <div className="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
+        <MovieCard movie={movie} />
       </div>
     ))}
   </div>
