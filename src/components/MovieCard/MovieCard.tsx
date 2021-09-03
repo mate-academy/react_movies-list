3a24b426d5266e5c651ec962047afcf03d115497
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-04 00:34:38 +0300

    fixed

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -6,37 +6,41 @@ interface Props {
   movie: Movie;
 }
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
-  <>
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={movie.imgUrl}
-            alt="Film logo"
-          />
-        </figure>
-      </div>
-    </div>
+export const MovieCard: React.FC<Props> = (props) => {
+  const { movie } = props;
 
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src={movie.imdbUrl} alt="" />
+  return (
+    <>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              src={movie.imgUrl}
+              alt="Film logo"
+            />
           </figure>
         </div>
+      </div>
 
-        <div className="media-content">
-          <p className="title is-8">{movie.title}</p>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src={movie.imdbUrl} alt="" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{movie.title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {movie.description}
+          <br />
+          <a href={movie.imdbUrl}>IMDB</a>
         </div>
       </div>
-
-      <div className="content">
-        {movie.description}
-        <br />
-        <a href={movie.imdbUrl}>IMDB</a>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
