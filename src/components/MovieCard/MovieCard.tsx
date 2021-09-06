21e36596922940e2ed7fce31a77494db0a5403ac
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-06 13:37:32 +0300

    fixed

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -10,16 +10,15 @@ export const MovieCard: React.FC<Props> = (props) => {
   const { movie } = props;
 
   return (
-    <>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img
-              src={movie.imgUrl}
-              alt="Film logo"
-            />
-          </figure>
-        </div>
+
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={movie.imgUrl}
+            alt="Film logo"
+          />
+        </figure>
       </div>
 
       <div className="card-content">
@@ -41,6 +40,6 @@ export const MovieCard: React.FC<Props> = (props) => {
           <a href={movie.imdbUrl}>IMDB</a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
