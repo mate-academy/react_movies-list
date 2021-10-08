f8daf3face3a2be162284a53df2d06ac4d448ed0
bohdan_kochetov <bodja1208@gmail.com>
2021-10-08 08:22:13 +0300

    solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  movies: MovieFromServer[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {
+        movies.map(item => (
+          <MovieCard data={item} />
+        ))
+      }
+    </div>
+  );
+};
---
+++ src/global.d.ts
@@ -0,0 +1,7 @@
+interface MovieFromServer {
+  title: string;
+  description?: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
