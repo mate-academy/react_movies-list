ffe89c822de1c1d20831ba0ad0e2d0037550155c
Kostiantyn Besedin <besconstantine@gmail.com>
2021-10-09 20:30:44 +0300

    Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { Movies } from '../../types/Movies';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export const MoviesList: React.FC<Movies> = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie: Movie) => (
+      <div key={movie.id} className="card">
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
 );
---
+++ src/types/Movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id: number;
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
