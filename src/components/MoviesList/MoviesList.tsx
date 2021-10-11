d39b4e4235bba97054e8c463dba68661ba6c1938
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-10-11 12:58:48 +0300

    Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import moviesFromServer from '../../api/movies.json';
 import './MoviesList.scss';
+import { Movie } from '../../../src/Types'
 
 export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+  <> 
+    <ul className="movies">
+      {moviesFromServer.map((movie:Movie) => <MovieCard {...movie} />)}
+    </ul>
+  </>
 );
