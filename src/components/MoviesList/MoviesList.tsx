25d56dc16cde483733370a77f7f5b8ecd742ee6f
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-11-04 22:11:22 +0200

    be

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../MovieCard';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export type Props = {
+  Props: Movie[],
+};
+
+export const MoviesList: React.FC<Props> = ({ Props }) => (
+  <>
+    {Props.map(film => (
+      <div className="card">
+        <MovieCard
+        key={film.imdbId}
+        title={film.title}
+        description={film.description}
+        imgUrl={film.imgUrl}
+        imdbUrl={film.imdbUrl}
+        imdbId={film.imdbId}
+      />
+      </div>
+    ))}
+  </>
 );
