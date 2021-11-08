a750cc7b56a6904a34a10a0af32b98accf21681a
Anton Karpena <antoniokarpena@gmail.com>
2021-11-08 08:51:01 +0200

    initial commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/types';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  moviesFromServer: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesFromServer }) => {
+  return (
+    <div className="movies">
+      {moviesFromServer.map((movie) => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  );
+};
