e34fc2bc9171ca9d63be168bd19e2a6ff63ab3b2
cyb1ove <cyb1ove.ua@gmail.com>
2021-11-11 20:31:53 +0300

    Task was solved

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,51 @@ import { MoviesList } from './components/MoviesList';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 
-export const App: React.FC = () => (
-  <div className="page">
-    <div className="page-content">
-      <MoviesList movies={moviesFromServer} />
-    </div>
-    <div className="sidebar">
-      Sidebar will be here
-    </div>
-  </div>
-);
+type Props = {};
+interface State {
+  query: string;
+}
+
+export class App extends React.Component<Props, State> {
+  state = {
+    query: '',
+  };
+
+  updateState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  hasSimilarInQuery = (...args: string[]) => {
+    return args.some(value => value.toLowerCase().includes(this.state.query));
+  };
+
+  render() {
+    const visibleMovies = moviesFromServer.filter(movie => {
+      const { title, description } = movie;
+
+      return this.hasSimilarInQuery(title, description);
+    });
+
+    return (
+      <>
+        <input
+          type="text"
+          className="input"
+          value={this.state.query}
+          onChange={(event) => this.updateState(event)}
+        />
+
+        <div className="page">
+          <div className="page-content">
+            <MoviesList movies={visibleMovies} />
+          </div>
+          <div className="sidebar">
+            Sidebar will be here
+          </div>
+        </div>
+      </>
+    )
+  }
+};
