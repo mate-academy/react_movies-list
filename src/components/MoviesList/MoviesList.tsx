a435ce6e09f43cb66629a7bedc9451dbf3bad89b
Lera Permiakova <vv.permiakova@gmail.com>
2021-12-04 21:54:10 +0200

    Movie

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { MovieInfo } from '../../types/MovieInfo';
+
+type Props = {
+  moviesCollection: MovieInfo[],
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesCollection }) => (
+  <div className="movies">
+    {moviesCollection.map(movie => (
+      <div className="card">
+        <MovieCard {...movie} key={movie.imdbId} />
+      </div>
+    ))}
+  </div>
 );
