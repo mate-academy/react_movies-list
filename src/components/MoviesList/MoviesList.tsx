ccc33b39c181943386c48e9e307ae70c6635ce6f
VladDyrenkov <59515329+VladDyrenkov@users.noreply.github.com>
2021-12-07 17:15:23 +0200

    Was added lost files

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../MovieCard/MovieType';
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({
+  movies,
+}) => (
+  <div className="movies">
+    {movies.map(({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    }) => (
+      <div key={imdbId}>
+        <MovieCard
+          title={title}
+          description={description}
+          imgUrl={imgUrl}
+          imdbUrl={imdbUrl}
+        />
+      </div>
+    ))}
+  </div>
 );
