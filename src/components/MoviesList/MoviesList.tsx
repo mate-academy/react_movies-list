7f3725677b07725f99587601f13c78e38dd8d8af
Mariia Bila <freiheit.maria@gmail.com>
2021-12-07 10:40:52 +0200

    movieList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+
+type List = {
+  title: string,
+  description?: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+};
+
+type Props = {
+  movies: List[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div className="card" key={movie.imdbId}>
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbUrl}
+          />
+        </div>
+      ))}
+    </div>
+  </>
 );
