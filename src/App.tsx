d9ecb9f4982629b5d8dff919d240f73058628130
Lera Litfullina <litfullina@icloud.com>
2021-12-13 17:15:39 +0300

    Movies list - Filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.scss';
 
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
 
-export const App: React.FC = () => (
-  <div className="page">
-    <div className="page-content">
-      <MoviesList movies={moviesFromServer} />
-    </div>
+export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = moviesFromServer.filter(movie => {
+    const queryInLower = query.toLowerCase();
+    const titleInLower = movie.title.toLowerCase();
+    const decriptionInLower = movie.description.toLowerCase();
 
-    <div className="sidebar">
-      Sidebar will be here
+    return (
+      titleInLower.includes(queryInLower) || decriptionInLower.includes(queryInLower)
+    );
+  });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="searchMovieBlock">
+          <h5 className="subtitle is-5">What do you want to watch?</h5>
+          <input
+            type="text"
+            className="input"
+            onChange={(event) => {
+              setQuery(event.target.value);
+            }}
+          />
+        </div>
+        <MoviesList movies={visibleMovies} />
+      </div>
+
+      <div className="sidebar">
+        Sidebar will be here
+      </div>
     </div>
-  </div>
-);
+  );
+};
