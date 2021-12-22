baabb160b2576dd3148d320593e865c56559d586
Andrey Grigoriev <nkgrand25@gmail.com>
2021-12-22 20:17:27 +0200

    Fixed card imdb image

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -21,7 +21,7 @@ export const MovieCard: React.FC<Props> = ({
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src={imdbUrl} alt="imdb" />
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
