7d7b3c0af116e6967fb8f42cff4b785eb99b80e6
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-29 12:53:06 +0200

    some problems with types

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+// interface ValidMovies {
+//   title: string;
+//   description?: string;
+//   imgUrl: string;
+//   imdbUrl: string;
+//   imdbId: string;
+// }
+
+export const MoviesList: React.FC<any> = ({ movies }) => (
+  <>
+    <ul className="movies">
+      {movies.map((movie: any) => (
+        <li className="card" key={movie.id}>
+          <MovieCard {...movie} />
+        </li>
+      ))}
+    </ul>
+  </>
 );
