fef80b717b9ac703f120a3e81598c9a66a3315d7
KHTRE <KHTREE@gmail.com>
2022-01-08 21:00:09 +0200

    works

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
-
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Movie = {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+};
+
+export const MovieCard: React.FC<Movie> = (props) => (
+  <>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={props.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{props.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {props.description}
+        <br />
+        <a href={props.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './MoviesList.scss';
+// import { SlowBuffer } from 'buffer';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = (props) => (
+  <ul className="movies">
+    {props.movies.map((movie: Movie) => (
+      <li className="card" key={movie.imdbId}>
+        <MovieCard {...movie} />
+      </li>
+    ))}
+  </ul>
 );
