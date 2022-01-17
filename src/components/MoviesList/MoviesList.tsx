f6513b20df93fd731c727395985dbc7d7f0b3a78
Roman Shevchuk <14rom.shevchuk@gmail.com>
2022-01-17 21:16:13 +0200

    react ML

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Card } from '../../types/Card';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Card[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div className="card" key={movie.imdbId}>
+          <MovieCard {...movie} />
+        </div>
+      ))}
+    </div>
+  </>
 );
