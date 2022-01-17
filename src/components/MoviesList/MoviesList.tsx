8e7186a62082c412ee7ba33ff646a2d1513f3fd5
Vova Alputov <alputov1998@gmail.com>
2022-01-17 18:00:23 +0200

    done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Movie {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    <div className="page">
+      <div className="page-content">
+        <div className="movies">
+          {movies.map(movie => (
+            <div className="card" key={movie.imdbId}>
+              <MovieCard {...movie} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="sidebar">
+        Sidebar will be here
+      </div>
+    </div>
+  </>
 );
