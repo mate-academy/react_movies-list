8d16dfbb391da034035fcb66894f28bae8d178ff
Marina Tilniak <marinatilnak@gmail.com>
2022-01-18 14:15:08 +0200

    Solution after review

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import { MoviesList } from './components/MoviesList';
 import './App.scss';
 
 export const App: React.FC = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={movies} />
-      </div>
-
-      <div className="sidebar">
-        Sidebar will be here
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MoviesList movies={movies} />
     </div>
-  </>
+
+    <div className="sidebar">
+      Sidebar will be here
+    </div>
+  </div>
 );
