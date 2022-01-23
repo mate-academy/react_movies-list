88f84fee63a902fba76ed49b199dfd40a3bd978a
Vasiliy <voronvasia@gmail.com>
2022-01-23 20:34:55 +0200

    task complete

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  moviesList: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesList }) => (
+  <>
+    <div className="movies">
+      {moviesList.map(movie => (
+        <MovieCard movie={movie} />
+      ))}
+    </div>
+  </>
 );
