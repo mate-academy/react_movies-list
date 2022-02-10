91b4e8ba02f617c0c3005144ddb1e6ef30136847
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-02-10 18:03:48 +0200

    done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface FilmWhithId {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+type Props = {
+  property: FilmWhithId[];
+};
+
+export const MoviesList: React.FC<Props> = ({ property = [] }) => (
+  <div className="movies">
+    {property.map(({
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+      imdbId,
+    }) => {
+      return (
+        <div id={imdbId}>
+          <MovieCard
+            title={title}
+            description={description}
+            imgUrl={imgUrl}
+            imdbUrl={imdbUrl}
+          />
+        </div>
+      );
+    })}
+  </div>
 );
