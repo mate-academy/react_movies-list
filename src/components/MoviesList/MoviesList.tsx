40201e7f281e0312bf9031c5244dd8d2229367f8
mariiaStorozhyk <xmsha722@gmail.com>
2022-02-11 16:22:42 +0200

    Solition

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <div>
+    <ul className="movies">
+      {movies.map(movie => (
+        <li key={movie.title}>
+          <MovieCard
+            {...movie}
+          />
+        </li>
+      ))}
+    </ul>
+  </div>
+
 );
---
+++ src/types/Movie.ts
@@ -0,0 +1,6 @@
+export interface Movie {
+  title: string;
+  description?: string;
+  imgUrl: string;
+  imdbUrl: string;
+}
