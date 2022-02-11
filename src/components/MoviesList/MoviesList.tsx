42f982866b7daba7989000e92a6c953d498b0893
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-02-11 16:27:24 +0300

    sol

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  cards: Movie[],
+};
+
+export const MoviesList: React.FC<Props> = ({ cards }) => (
+  <ul className="movies">
+    {cards.map(card => (
+      <li key={card.imdbId}>
+        <MovieCard
+          title={card.title}
+          description={card.description}
+          imgUrl={card.imgUrl}
+          imdbUrl={card.imdbUrl}
+        />
+      </li>
+    ))}
+  </ul>
 );
