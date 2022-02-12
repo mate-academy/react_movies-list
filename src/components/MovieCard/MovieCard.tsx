36fb1c18f49ec15b3d654969ef23e959ded1a524
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-02-12 13:58:07 +0200

    first

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,6 +2,48 @@ import React from 'react';
 
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+export interface Movie {
+  title: string;
+  description: string;
+  imgURL: string;
+  imdbURL: string;
+  imdbId: string;
+}
+
+export const MovieCard: React.FC<Movie> = ({
+  title,
+  description,
+  imgURL,
+  imdbURL,
+}) => (
+  <>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={imgURL}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {description}
+        <br />
+        <a href={imdbURL}>IMDB</a>
+      </div>
+    </div>
+  </>
 );
