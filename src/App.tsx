314f580da50ecfff8e9a12995c55ab8e427085ff
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-02-14 15:23:06 +0200

    fixes after code review

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
-// import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => (
   <div className="page">
