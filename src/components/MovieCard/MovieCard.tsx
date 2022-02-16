7c4b94ca54748dbc773b0245d3d64adf8a37e965
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-02-16 11:47:13 +0200

    second solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -3,41 +3,44 @@ import React from 'react';
 import './MovieCard.scss';
 import { Film } from '../../types/film';
 
-export const MovieCard: React.FC<Film> = ({
+type Props = Film;
+
+export const MovieCard: React.FC<Props> = ({
   title,
   description = '',
   imgUrl,
   imdbUrl,
 }) => (
-  // eslint-disable-next-line react/jsx-indent
-  <div className="card">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          src={imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div>
-
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
-          </figure>
-        </div>
-
-        <div className="media-content">
-          <p className="title is-8">{title}</p>
-        </div>
+  <>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
       </div>
 
-      <div className="content">
-        {description}
-        <br />
-        <a href={imdbUrl}>IMDB</a>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl}>IMDB</a>
+        </div>
       </div>
     </div>
-  </div>
+  </>
 );
---
+++ src/types/film.ts
@@ -0,0 +1,7 @@
+export interface Film {
+  title: string;
+  description?: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
