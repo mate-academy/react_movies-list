5de414531924af39c940bf59dfd1963566ba14e5
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-09 19:53:55 +0200

    works

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  movies: Movie[],
+};
+
+export const MoviesList: React.FC<Props> = (props) => {
+  const { movies } = props;
+
+  return (
+    <>
+      <ul className="movies">
+        {
+          movies.map(movie => (
+            <li className="card" key={movie.imdbId}>
+              <MovieCard movie={movie} />
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
