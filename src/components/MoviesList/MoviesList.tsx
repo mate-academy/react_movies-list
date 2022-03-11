e1b8a7a1f55a192f3c84e73d8a6aeaa68b8e45d2
Anna Serdechna <anserdechna@gmail.com>
2022-03-11 22:19:48 +0200

    solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+
+import { Movie } from '../../types/movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Movies = {
+  // eslint-disable-next-line react/require-default-props
+  movies?: Movie[];
+};
+
+export const MoviesList: React.FC<Movies> = ({ movies }) => (
+  <ul className="movies">
+    {movies && movies.map(movie => (
+      <li key={movie.imdbUrl}>
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imdbUrl={movie.imdbUrl}
+          imgUrl={movie.imgUrl}
+        />
+      </li>
+    ))}
+  </ul>
 );
---
+++ src/types/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  title: string;
+  // eslint-disable-next-line react/require-default-props
+  description?: string;
+  imgUrl: string;
+  imdbUrl: string;
+}
