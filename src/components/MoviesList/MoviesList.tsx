7fa2fde58fd7e77ba2fef833ea968f0529935cc8
Pasechnik Vitaly <pasechnik.vitaly.11@gmail.com>
2022-03-15 12:06:00 +0200

    last saved

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard/MovieCard';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  moviesFromServer: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesFromServer = [] }) => (
+  <div className="movies">
+    <ul>
+      { moviesFromServer.map(movie => (
+        <li
+          key={movie.imdbId}
+        >
+          <MovieCard {...movie} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
