1abec9978520a90101ead1e5304c6545155cd7a3
Dmytro Pelovych <falcon.patron@gmail.com>
2022-03-19 15:38:35 +0200

    Solution for `React Movies List`

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MoviesListProps = {
+  movies: Movie[],
+};
+
+export const MoviesList: React.FC<MoviesListProps> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => <MovieCard {...movie} />)}
+  </div>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
-
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
