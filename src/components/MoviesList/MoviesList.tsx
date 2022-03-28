da9bc9c6c4d7fd4c26e9904c1cbee27736f95c97
Nazar Ostafiichuk <ostafii4uk@gmail.com>
2022-03-28 16:48:01 +0300

    Fixes after review

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -8,17 +8,15 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map((movie) => (
-        <MovieCard
-          title={movie.title}
-          description={movie.description}
-          imgUrl={movie.imgUrl}
-          imdbUrl={movie.imdbUrl}
-          imdbId={movie.imdbId}
-        />
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map((movie) => (
+      <MovieCard
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+        imdbId={movie.imdbId}
+      />
+    ))}
+  </div>
 );
