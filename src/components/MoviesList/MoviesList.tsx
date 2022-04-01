767e6b62f621b7b4a2d5350e25d9338ec515b666
KristinaTar <kristina.tarashchuk@gmail.com>
2022-04-01 16:20:22 +0300

    done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+}
+ type Props = {
+   serverData: Movie[];
+ };
+
+export const MoviesList: React.FC<Props> = ({ serverData = [] }) => {
+  return (
+    <div className="page-content">
+      <div className="movies">
+        {serverData.map(e => <MovieCard {...e} />)}
+      </div>
+    </div>
+  );
+};
