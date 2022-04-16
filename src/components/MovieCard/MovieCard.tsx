235c34c189e0a6ba4c1fd732b98326b9e70bc783
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-04-16 17:05:10 +0300

    The solution was implemented

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
-
+import { Movie } from '../../types/Movie';
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+/* eslint-disable max-len */
+export const MovieCard: React.FC<Movie> = (cardMovie) => (
+  <>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={cardMovie.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+        <div className="media-content">
+          <p className="title is-8">
+            {cardMovie.title}
+          </p>
+        </div>
+      </div>
+      <div className="content">
+        {cardMovie.description}
+        <br />
+        <a href={cardMovie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type ListOfMovies = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<ListOfMovies> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => {
+        return (
+          <div className="card" key={movie.imdbId}>
+            <MovieCard {...movie} />
+          </div>
+        );
+      })}
+    </div>
+  </>
 );
