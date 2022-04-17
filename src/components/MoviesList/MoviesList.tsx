e538b3e0792503fab3c809f810ff345303ef91ba
unknown <xpromt@gmail.com>
2022-04-17 17:17:11 +0300

    Result

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieType } from '../../types/MovieType';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies : MovieType[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          filmTitle={movie.title}
+          filmDescription={movie.description}
+          filmImgUrl={movie.imgUrl}
+          filmImdbUrl={movie.imdbUrl}
+          filmImdbId={movie.imdbId}
+        />
+      ))}
+    </div>
+  </>
 );
