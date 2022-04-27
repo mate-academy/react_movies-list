bdb21163bbba7ceb20a0c791cc762cad469669c6
Den <81646849+DenCoderok@users.noreply.github.com>
2022-04-27 18:48:41 +0300

    movies list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+interface MoviesCard {
+  title:string;
+  description?:string;
+  imgUrl:string;
+  imdbUrl:string;
+  imdbId:string;
+}
+
+type Props = {
+  moviesData: MoviesCard[]
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesData }) => {
+  const keyId = 0;
+
+  return (
+    <div className="movies">
+
+      {
+        moviesData.map(moviesItem => (
+          <div className="card" key={keyId + 1}>
+            <MovieCard {...moviesItem} />
+          </div>
+        ))
+      }
+
+    </div>
+  );
+};
