1626d58370bbae9c78ba415aa5973c2f69a0f30a
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-04-28 14:47:45 +0300

    MoliesList was created

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Movie } from '../types/Movie';
+
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <ul>
+    {movies.map(movie => (
+      <li key={movie.imdbId}>
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      </li>
+    ))}
+  </ul>
 );
