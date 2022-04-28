52ba419c010718d2da40f08773580617269d10ce
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2022-04-28 19:28:10 +0300

    Second commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
   <ul className="movies">
     {movies.map(movie => (
-      <li key={movie.imdbId} className="movie">
+      <li key={movie.imdbId}>
         <MovieCard
           title={movie.title}
           description={movie.description}
