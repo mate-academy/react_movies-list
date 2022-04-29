fb4d3bebfdcdf3693b3e56b5b48f41c4b280a06b
Ruslana Tomnyuk <rusia.vasia1@gmail.com>
2022-04-29 20:39:07 +0300

    react_movies-list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+
+import { Movie } from '../../types/Movie';
+
+import { MovieCard } from '../MovieCard';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div>
+          {movie.title}
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbUrl}
+          />
+        </div>
+      ))}
+    </div>
+  </>
 );
