d0c0a9a71fcbabb8064798183e7d29387335492e
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-04-29 22:15:51 +0200

    react_movies-list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
-import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import './MoviesList.scss';
+import { MovieCardItem } from '../MovieCard';
+
+interface MovieCard {
+  title: string,
+  description?: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+type Props = {
+  movies: MovieCard[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" key={movie.imdbId}>
+        <MovieCardItem
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbId}
+        />
+      </div>
+    ))}
+  </div>
 );
