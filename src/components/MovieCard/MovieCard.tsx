bcd71a378918f602fc627e01e6d88fe3dc52b9dd
SeKaM0 <polozhentsew02@gmail.com>
2022-04-30 21:12:59 +0300

    Finish task

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Props = {
+  title: string;
+  description?: string;
+  img: string;
+  imdb: string;
+
+};
+
+export const MovieCard: React.FC<Props> = ({
+  title,
+  description,
+  img,
+  imdb,
+}) => (
+  <div className="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={img}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {description}
+        <br />
+        <a href={imdb}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
+
+MovieCard.defaultProps = {
+  description: '',
+};
+
+MovieCard.propTypes = {
+  description: PropTypes.string,
+};
