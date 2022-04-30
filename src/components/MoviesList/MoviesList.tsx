ef6a042aaaa99411bd8b675290bf7e8997426b75
Anton Vakoliuk <antonvakolyuk2001@gmail.com>
2022-04-30 13:29:42 +0200

    MoviesList was created

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => {
+      return (
+        <div className="card" key={movie.imdbId}>
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbId}
+          />
+        </div>
+      );
+    })}
+  </div>
 );
