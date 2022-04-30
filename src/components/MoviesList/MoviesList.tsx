0af954456c9eec6bc7e2f81e6ac0cae7cc0003c2
Misha Kravtsov <aniahucm@gmail.com>
2022-05-01 01:44:25 +0300

    amked components movieList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { MoviesServerData } from '../../types/MoviesServerData';
+import { MovieCard } from '../MovieCard';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type ServerData = {
+  moviesData: Array<MoviesServerData>
+};
+
+export const MoviesList: React.FC<ServerData> = ({ moviesData }) => (
+  <div className="moviesList">
+    {moviesData.map(({
+      title, description, imgUrl, imdbUrl,
+    // eslint-disable-next-line array-callback-return
+    }) => (
+      <MovieCard
+        title={title}
+        description={description}
+        imgUrl={imgUrl}
+        imdbUrl={imdbUrl}
+      />
+    ))}
+  </div>
 );
