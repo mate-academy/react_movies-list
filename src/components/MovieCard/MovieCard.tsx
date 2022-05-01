b85fd4eb8d5219eb89e2da2a8920b8506c672ff9
yurii837 <gorogotskiy@gmail.com>
2022-05-01 20:19:06 +0300

    done

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
-
 import './MovieCard.scss';
+import { MovieType } from '../Types/MovieType';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Props = {
+  movieData: MovieType
+};
+
+export const MovieCard: React.FC<Props> = ({ movieData }) => (
+  <>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={movieData.imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{movieData.title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {movieData.description}
+          <br />
+          <a href={movieData.imdbUrl}>{movieData.imdbId}</a>
+        </div>
+      </div>
+    </div>
+  </>
+
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { MovieType } from '../Types/MovieType';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  data: MovieType[]
+};
+
+export const MoviesList: React.FC<Props> = ({ data }) => (
+  <div className="movies">
+    {data.map(movieObj => <MovieCard movieData={movieObj} />)}
+  </div>
 );
