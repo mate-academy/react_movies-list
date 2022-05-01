42940c06e9e7785dcfd15c2922a1a777f14522d9
Helen Petrovska <paznachka@gmail.com>
2022-05-01 11:45:23 +0300

    Movies wwere created

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" key={movie.imdbId}>
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imdbUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      </div>
+    ))}
+  </div>
 );
