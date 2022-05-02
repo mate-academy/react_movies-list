0ced160e478596a8b333f29938f70b85cba634eb
Vlad Skoromnyi <VectraSton@gmail.com>
2022-05-02 12:03:07 +0300

    complete

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Movie {
+  title: string,
+  description?: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string
+}
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(({
+      title, description, imgUrl, imdbUrl, imdbId,
+    }) => (
+      <MovieCard
+        key={imdbId}
+        title={title}
+        description={description}
+        imgUrl={imgUrl}
+        imdbUrl={imdbUrl}
+      />
+    ))}
+  </div>
 );
