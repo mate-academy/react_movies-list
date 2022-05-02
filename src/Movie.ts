f26a75afd2d427c18751d3956f580693d1fad429
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-05-02 12:29:12 +0300

    movies

---
+++ src/Movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+  title: string;
+  description: string;
+  imgURL: string;
+  imdbURL: string;
+  imdbId: string;
+}
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,6 +2,51 @@ import React from 'react';
 
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Props = {
+  title: string;
+  description?: string;
+  imgURL: string;
+  imdbURL: string;
+};
+
+export const MovieCard: React.FC<Props> = ({
+  title,
+  description,
+  imgURL,
+  imdbURL,
+}) => (
+  <>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={imgURL}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {description}
+        <br />
+        <a href={imdbURL}>IMDB</a>
+      </div>
+    </div>
+  </>
 );
+
+MovieCard.defaultProps = {
+  description: 'No description',
+};
