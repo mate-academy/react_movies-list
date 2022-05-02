c49e9a88f0d6bc64c632bfbd390feb81fb463bdf
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-05-02 17:19:15 +0300

    craete task

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Movie } from '../../types/movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  moviesList: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesList }) => (
+  <div className="movies">
+    {moviesList.map(item => (
+      <MovieCard
+        key={item.imdbId}
+        title={item.title}
+        description={item.description}
+        imdbUrl={item.imdbUrl}
+        imgUrl={item.imgUrl}
+      />
+    ))}
+  </div>
 );
