2f2a220e5ac22d8575ea6253cab4b76078d167d4
WyLNdOoa <GreyIl@protonmail.com>
2022-05-03 00:20:35 +0300

    task done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  movieList: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movieList = [] }) => (
+  <div className="movies">
+    {movieList.map(movie => (
+      <MovieCard
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+      />
+    ))}
+  </div>
 );
