c7c1ab0645e2e8d22e883b43993143fe74a2d2b7
Andriy Shvets <PiratesQ@i.ua>
2022-05-03 16:17:59 +0300

    changed div to ul li

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -8,17 +8,19 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
+  <ul className="movies">
     {movies.map(({
       title, description, imgUrl, imdbUrl, imdbId,
     }) => (
-      <MovieCard
-        key={imdbId}
-        title={title}
-        description={description}
-        imgUrl={imgUrl}
-        imdbUrl={imdbUrl}
-      />
+      <li key={imdbId}>
+        <MovieCard
+          key={imdbId}
+          title={title}
+          description={description}
+          imgUrl={imgUrl}
+          imdbUrl={imdbUrl}
+        />
+      </li>
     ))}
-  </div>
+  </ul>
 );
