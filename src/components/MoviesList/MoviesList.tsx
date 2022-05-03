6081d3e89da929c7b70218f365614d9dab6a2200
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-03 20:28:16 +0300

    Move <li> to moviecard

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,9 +11,10 @@ export const MoviesList: React.FC<Props> = ({ movies }) => (
   <ul className="movies">
     {
       movies.map((movie) => (
-        <li className="card" key={movie.imdbId}>
-          <MovieCard movie={movie} />
-        </li>
+        <MovieCard
+          movie={movie}
+          key={movie.imdbId}
+        />
       ))
     }
   </ul>
