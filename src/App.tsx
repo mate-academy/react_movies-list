f2b04626120f19cf1b762bbe47b2d9cf6dc96fd7
Ruslana Tomnyuk <rusia.vasia1@gmail.com>
2022-05-03 17:20:08 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { MoviesList } from './components/MoviesList';
 
 import moviesFromServer from './api/movies.json';
 
-/* eslint-disable max-len */
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
+import './MoviesList.scss';
 
 import { Movie } from '../../types/Movie';
 
 import { MovieCard } from '../MovieCard';
-
-import './MoviesList.scss';
 
 type Props = {
   movies: Movie[];
 };
 
 export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie => (
-        <div>
-          <MovieCard
-            title={movie.title}
-            description={movie.description}
-            imgUrl={movie.imgUrl}
-            imdbUrl={movie.imdbUrl}
-          />
-        </div>
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map(movie => (
+      <div
+        className="card"
+        key={movie.imdbId}
+      >
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      </div>
+    ))}
+  </div>
 );
