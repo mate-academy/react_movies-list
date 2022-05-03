29af20a3452fb1af616f8d197318579e08f4da66
WyLNdOoa <GreyIl@protonmail.com>
2022-05-03 15:28:58 +0300

    commit message

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -4,17 +4,16 @@ import './MovieCard.scss';
 
 import { Movie } from '../../types/Movie';
 
-export const MovieCard: React.FC<Movie> = ({
-  title,
-  description,
-  imgUrl,
-  imdbUrl,
-}) => (
+type Props = {
+  movie: Movie,
+};
+
+export const MovieCard: React.FC<Props> = ({ movie }) => (
   <div className="card">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
-          src={imgUrl}
+          src={movie.imgUrl}
           alt="Film logo"
         />
       </figure>
@@ -29,14 +28,14 @@ export const MovieCard: React.FC<Movie> = ({
         </div>
 
         <div className="media-content">
-          <p className="title is-8">{title}</p>
+          <p className="title is-8">{movie.title}</p>
         </div>
       </div>
 
       <div className="content">
-        {description}
+        {movie.description}
         <br />
-        <a href={imdbUrl}>IMDB</a>
+        <a href={movie.imdbUrl}>IMDB</a>
       </div>
     </div>
   </div>
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -5,18 +5,20 @@ import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
 
 type Props = {
-  movieList: Movie[]
+  movieList: Movie[],
 };
 
 export const MoviesList: React.FC<Props> = ({ movieList = [] }) => (
   <div className="movies">
-    {movieList.map(movie => (
-      <MovieCard
-        title={movie.title}
-        description={movie.description}
-        imgUrl={movie.imgUrl}
-        imdbUrl={movie.imdbUrl}
-      />
-    ))}
+    {
+      movieList.map(movie => (
+        <div className="card">
+          <MovieCard
+            key={movie.imdbId}
+            movie={movie}
+          />
+        </div>
+      ))
+    }
   </div>
 );
