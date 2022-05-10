2b5c4f577d9278ae8cb9ac661d15c14b043cbfa3
unknown <avicostov2@gmail.com>
2022-05-10 13:43:24 +0200

    solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import MoviesFromServer from '../../api/movies.json';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
 export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+  <div className="movies">
+    {
+      MoviesFromServer.map(({
+        title, description, imgUrl, imdbUrl, imdbId,
+      }) => (
+        <MovieCard
+          title={title}
+          description={description}
+          imgUrl={imgUrl}
+          imdbUrl={imdbUrl}
+          imdbId={imdbId}
+        />
+      ))
+    }
+  </div>
 );
