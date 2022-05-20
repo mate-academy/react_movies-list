c52935eb4f2b749a0a808ecc00f02c0901b79ada
Bohdan-Dubravin <dubravin111@gmail.com>
2022-05-20 21:18:46 +0300

    Solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
-
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
-);
+interface Props {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+};
+
+export const MovieCard: React.FC<Props> = (props) => {
+  const { title, description, imgUrl, imdbUrl } = props;
+
+  return (
+    <>
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="../images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{title}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl}>IMDB</a>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface List {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+type Props = {
+  datafromServer: List[]
+};
+
+export const MoviesList: React.FC<Props> = ({ datafromServer = [] }) => (
+  <div className="movies">
+    {datafromServer.map(movie => (
+      <div key={movie.imdbId} className="card">
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
 );
