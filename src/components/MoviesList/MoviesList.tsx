9a2ca1c46296acd230fe07e599d03542e0956f20
Oleksandr Strilchuk <virass.m@gmail.com>
2022-05-25 00:15:53 +0300

    Task complete

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../Types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Movies {
+  movies: Movie[];
+}
+export const MoviesList: React.FC<Movies> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div key={movie.imdbId} className="card">
+          <MovieCard movie={movie} />
+        </div>
+      ))}
+    </div>
+  </>
 );
