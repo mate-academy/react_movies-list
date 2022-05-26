91225332e19096617b8a39d7e62c8c14f252bbe4
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-05-26 17:14:12 +0200

    First try

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
-/// <reference types="react-scripts" />
+// / <reference types="react-scripts" />
+
+interface MovieCardProps {
+  film: Films,
+}
+
+interface Films {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+interface Props {
+  films: Films[]
+}
