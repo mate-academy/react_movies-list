a2d4522721c5e9d54f99491a09225880dd7addbc
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-05-26 21:59:35 +0300

    Movies was done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface FilmProp {
+  title: string;
+  description: string;
+  imbdUrl: string;
+  imgUrl: string;
+  imdbId: string;
+}
+
+interface FilmList {
+  movieList: FilmProp[];
+}
+
+export const MoviesList: React.FC<FilmList> = ({ movieList = [] }) => (
+
+  <>
+    <div className="movies">
+      {movieList.map(film => (
+        <MovieCard
+          title={film.title}
+          description={film.description}
+          imbdUrl={film.imbdUrl}
+          imgUrl={film.imgUrl}
+          imdbId={film.imdbId}
+        />
+      ))}
+    </div>
+  </>
 );
