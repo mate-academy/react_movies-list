d28391397bf23c482d6b2bd3cc08109ec5a79601
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-05-26 18:02:06 +0300

    fixed

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
-
-interface Movie {
-  title: string,
-  description: string,
-  imgUrl: string,
-  imdbUrl: string,
-  imdbId: string,
-}
+import { Movie } from '../../react-app-env';
 
 type Props = {
   movies: Movie[]
