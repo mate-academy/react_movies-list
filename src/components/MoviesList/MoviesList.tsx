c23aec0df038a17952bd5ed88b767a81f8ddb6ff
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-05-27 21:52:02 +0300

    movie

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../react-app-env';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export const MoviesList: React.FC<{ movies: Movie[]}> = ({
+  movies,
+}: { movies: Movie[] }) => (
+  <div className="movies">
+    {movies.map((mov: Movie) => (
+      <MovieCard {...mov} />
+    ))}
+  </div>
 );
