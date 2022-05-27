2b753f902c5e8a8c0f4bc2bf7d3bb6131e5e71ed
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-27 14:09:26 +0300

    task are done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movies } from '../../react-app-env';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Props {
+  movies: Movies[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movies={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
