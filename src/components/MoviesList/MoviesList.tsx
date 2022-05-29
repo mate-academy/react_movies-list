b5def46d5f8c368c27902276f9b4df89f615818f
Ihor Yeremenko <igoree86@gmail.com>
2022-05-29 21:06:15 +0300

    First_commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,31 @@
+/* eslint-disable react/jsx-fragments */
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <React.Fragment key={movie.imdbId}>
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      </React.Fragment>
+    )) }
+  </div>
 );
+
+interface Movie {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string,
+  imdbId: string,
+}
+
+type Props = {
+  movies: Movie[],
+};
