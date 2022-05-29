fd38772a4d1c2f43e1e18e4b8044fada8d40b383
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-29 16:29:46 +0200

    move movies

--- src/Types/Movie.ts
+++ src/Types/Movie.ts
@@ -1,5 +1,3 @@
-/// <reference types="react-scripts" />
-
 export interface Movie {
   title: string;
   description: string;
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Movie } from '../../react-app-env';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+interface Props {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map((movie) => {
+        return (
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbUrl}
+            imdbId={movie.imdbId}
+          />
+        );
+      })}
+    </div>
+  );
+};
