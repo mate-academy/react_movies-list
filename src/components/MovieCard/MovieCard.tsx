0b31dd595c677df1a592eea289b3aec4a20b6be8
Daryna Chekmariova <dashachekmariova@gmail.com>
2022-05-30 19:27:32 +0300

    React movies-list

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
-
+import { Movies } from '../../react-app-env';
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+export const MovieCard: React.FC<Movies> = ({
+  title,
+  description,
+  imgUrl,
+  imbdUrl,
+  imdbId,
+}) => (
+  <div className="card" data-cy="card" key={imdbId}>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          // eslint-disable-next-line max-len
+          src={imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {description}
+        <br />
+        <a href={imbdUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movies } from '../../react-app-env';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movies[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    {movies.map(movieBlock => {
+      const {
+        title,
+        description,
+        imgUrl,
+        imbdUrl,
+        imdbId,
+      } = movieBlock;
+
+      return (
+        <MovieCard
+          title={title}
+          description={description}
+          imgUrl={imgUrl}
+          imbdUrl={imbdUrl}
+          imdbId={imdbId}
+        />
+      );
+    })}
+  </>
 );
