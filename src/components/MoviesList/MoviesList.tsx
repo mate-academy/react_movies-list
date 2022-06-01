999fee3453f1b4176d84b93ee2771c5a3dd9222f
Andriy Simyachenko <simyachenko@gmail.com>
2022-06-01 11:33:34 +0300

    commit message

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
+import { Movie } from '../../react-app-env';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  films: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ films }) => (
+  <div className="movies">
+    {films.map((film) => (
+      <MovieCard film={film} />
+    ))}
+  </div>
 );
