1460724c246ebfbcffc4a79c322cea4295a1937f
Lebogang Mogane <mogane420@gmail.com>
2022-06-01 14:58:03 +0200

    Initial commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface Movie {
+  movies: [];
+}
+
+export const MoviesList: React.FC<Movie> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <div className="card" data-cy="card" key={movie.imdbId}>
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbUrl}
+          />
+        </div>
+      ))}
+    </div>
+  </>
 );
