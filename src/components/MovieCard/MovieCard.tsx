899d43daac731b4745d03489271924c4fa3f7f79
MabuzaM <mabuzamfano@gmail.com>
2022-06-02 10:49:24 +0200

    Task Complete

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
-import { MoviesList } from '../MoviesList';
-import { Movies } from '../../appTypeDefs';
+// import { MoviesList } from '../MoviesList';
+// import { Movies } from '../../appTypeDefs';
 import './MovieCard.scss';
 
-type MovieCardProps = {
-  movies: Movies[];
-};
+interface MovieCardProps {
+  title: string;
+  imdbUrl: string;
+  imgUrl: string;
+  description: string;
+}
 
-export const MovieCard: React.FC<MovieCardProps> = (movies) => {
+export const MovieCard: React.FC<MovieCardProps> = ({
+  title,
+  description,
+  imdbUrl,
+  imgUrl,
+}) => {
   return (
     <>
-      <MoviesList {...movies} />
+      <div className="card" data-cy="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+            // eslint-disable-next-line
+              src={ imgUrl }
+              alt="Film logo"
+            />
+          </figure>
+        </div>
+
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8">{ title }</p>
+            </div>
+          </div>
+
+          <div className="content">
+            { description }
+            <br />
+            <a href={`${imdbUrl}`}>IMDB</a>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,57 +1,26 @@
 import React from 'react';
 import './MoviesList.scss';
-import { Movies } from '../../appTypeDefs';
+import { Movie } from '../../appTypeDefs';
+import { MovieCard } from '../MovieCard';
 
-type MoviesProps = {
-  movies: Movies[];
+type MoviesListProps = {
+  movies: Movie[];
 };
 
-export const MoviesList: React.FC<MoviesProps> = (
-  { movies = [] },
+export const MoviesList: React.FC<MoviesListProps> = (
+  { movies },
 ) => {
   return (
-    <>
-      {movies.map((
-        {
-          title,
-          imdbId,
-          imdbUrl,
-          imgUrl,
-          description,
-        },
-      ) => (
-        <div className="card" data-cy="card" key={imdbId}>
-          <div className="card-image">
-            <figure className="image is-4by3">
-              <img
-              // eslint-disable-next-line
-                src={ imgUrl }
-                alt="Film logo"
-              />
-            </figure>
-          </div>
-
-          <div className="card-content">
-            <div className="media">
-              <div className="media-left">
-                <figure className="image is-48x48">
-                  <img src="images/imdb-logo.jpeg" alt="imdb" />
-                </figure>
-              </div>
-
-              <div className="media-content">
-                <p className="title is-8">{ title }</p>
-              </div>
-            </div>
-
-            <div className="content">
-              { description }
-              <br />
-              <a href={`${imdbUrl}`}>IMDB</a>
-            </div>
-          </div>
-        </div>
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          title={movie.title}
+          description={movie.description}
+          imdbUrl={movie.imdbUrl}
+          imgUrl={movie.imgUrl}
+        />
       ))}
-    </>
+    </div>
   );
 };
