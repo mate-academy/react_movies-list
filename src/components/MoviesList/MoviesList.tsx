aaf9bddb0270a43115dfbd8fa20dd6b16b496305
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2022-06-02 18:38:49 +0200

    refactored to not pass unused properties

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -6,14 +6,7 @@ import { MovieList } from '../../types/movieList';
 export const MoviesList: React.FC<MovieList> = ({ movies }) => (
   <div className="movies">
     {movies.map((movie) => (
-      <MovieCard
-        key={movie.imdbId}
-        title={movie.title}
-        description={movie.description}
-        imgUrl={movie.imgUrl}
-        imdbUrl={movie.imdbUrl}
-        imdbId={movie.imdbId}
-      />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
