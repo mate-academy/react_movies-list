33d5357de91daad1d97060665e5bcb311f07b263
Lilia Mazur <liliamazur2508@gmail.com>
2022-06-03 10:37:51 +0300

    fixed

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -9,13 +9,7 @@ interface Props {
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard
-        title={movie.title}
-        description={movie.description}
-        imgUrl={movie.imgUrl}
-        imdbUrl={movie.imdbUrl}
-        imdbId={movie.imdbId}
-      />
+      <MovieCard {...movie} />
     ))}
   </div>
 );
