dbac85d9d654b4291c7eb21ef35451318365540e
Lebogang Mogane <mogane420@gmail.com>
2022-06-03 11:11:57 +0200

    Moved types to their own file, removed unneccessary fragments

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,32 +1,27 @@
 import React from 'react';
 import { MovieCard } from '../MovieCard';
+import { Movie } from '../../react-app-env';
 import './MoviesList.scss';
 
-interface Keys {
-  title: string;
-  description: string;
-  imdbId: string;
-  imgUrl: string;
-  imdbUrl: string;
+interface Props {
+  movies: Movie[];
 }
 
-interface Movie {
-  movies: Keys[];
-}
-
-export const MoviesList: React.FC<Movie> = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(movie => (
-        <div className="card" data-cy="card" key={movie.imdbId}>
-          <MovieCard
-            title={movie.title}
-            description={movie.description}
-            imgUrl={movie.imgUrl}
-            imdbUrl={movie.imdbUrl}
-          />
-        </div>
-      ))}
-    </div>
-  </>
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div
+        className="card"
+        data-cy="card"
+        key={movie.imdbId}
+      >
+        <MovieCard
+          title={movie.title}
+          description={movie.description}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+        />
+      </div>
+    ))}
+  </div>
 );
