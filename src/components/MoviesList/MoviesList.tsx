c44a86e33fbc5ddc3b6511503e24c4c7b71c7918
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-06-03 09:59:31 +0200

    react movie

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../types';
+
+interface Props {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <>
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li key={movie.imdbId}>
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imdbUrl={movie.imdbUrl}
+            imgUrl={movie.imgUrl}
+          />
+        </li>
+      ))}
+    </ul>
+  </>
 );
---
+++ src/components/types/movie.types.tsx
@@ -0,0 +1,7 @@
+export interface Movie {
+  imgUrl: string;
+  title: string;
+  description: string;
+  imdbUrl: string;
+  imdbId: string;
+}
