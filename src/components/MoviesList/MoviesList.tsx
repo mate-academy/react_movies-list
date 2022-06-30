a60c58b02970407646f48e639bd05b100e6307d5
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-06-30 17:09:45 +0300

    fixed

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-interface Movie {
-  title: string,
-  description: string,
-  imgUrl: string,
-  imdbUrl: string,
-  imdbId: string,
-}
-
 type Props = {
   movies: Movie[],
 };
@@ -17,8 +9,12 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <ul className="movies">
     {movies.map(movie => (
-      <li className="card" data-cy="card" key={movie.imdbId}>
-        <MovieCard {...movie} />
+      <li
+        className="card"
+        data-cy="card"
+        key={movie.imdbId}
+      >
+        <MovieCard movie={movie} />
       </li>
     ))}
   </ul>
