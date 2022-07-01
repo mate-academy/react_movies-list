6d7ecb4e96fdf9ab3cac501c541f4d7c9907247d
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-07-01 17:40:27 +0300

    Key added

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -18,6 +18,7 @@ export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard
+        key={movie.imdbId}
         title={movie.title}
         description={movie.description}
         imgUrl={movie.imgUrl}
