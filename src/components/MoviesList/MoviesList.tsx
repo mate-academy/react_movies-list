b973bf0475111ffd0fe09a1c46a170cf1e576763
SAMAEV <andreysamaev@gmail.com>
2022-07-04 18:37:17 +0300

    myMoviesList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../Types/Movie'
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map(({
+        description, imdbUrl, imgUrl, title, imdbId,
+      }) => {
+        return (
+          <MovieCard
+            key={imdbId}
+            title={title}
+            description={description}
+            imgUrl={imgUrl}
+            imdbUrl={imdbUrl}
+          />
+        );
+      })}
+    </div>
+  );
+};
