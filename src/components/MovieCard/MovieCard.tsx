6239dc3e16bb162de772b73c9446e7f6c97e3117
Alina Komashchuk <akomashchuk24@gmail.com>
2022-07-04 21:12:25 +0300

    solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Card } from '../Types/Card';
 
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+export const MovieCard: React.FC<{ card: Card }> = ({ card }) => (
+  <div className="card" data-cy="card" key={card.imdbId}>
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={card.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{card.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {card.description}
+        <br />
+        <a href={card.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Card } from '../Types/Card';
+
+export const MoviesList: React.FC<{ list: Card[] }> = ({ list }) => (
+  <div className="movies">
+    {list.map(card => (
+      <MovieCard card={card} />
+    ))}
+  </div>
 );
