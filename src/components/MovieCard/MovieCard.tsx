da27c7bd0fdababed9f04d673a51d62045e8797b
SpaceHook <maxdumiak39@gmail.com>
2022-07-04 15:42:33 +0300

    React movies - ready

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
-
 import './MovieCard.scss';
+import { Card } from '../../types/Card';
+
+import moviesFromServer from '../../api/movies.json';
+
+const cards: Card[] = moviesFromServer.map(card => ({
+  ...card,
+}));
 
 export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+  <>
+    {cards.map(card => (
+      <div className="card" data-cy="card" key={card.imdbId}>
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              // eslint-disable-next-line max-len
+              src={card.imgUrl}
+              alt="Film logo"
+            />
+          </figure>
+        </div>
+
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8">{card.title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            {card.description}
+            <br />
+            <a href={card.imdbUrl}>IMDB</a>
+          </div>
+        </div>
+      </div>
+    ))}
+  </>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
 export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+  <>
+    <div className="page">
+      <div className="page-content">
+        <div className="movies">
+          <MovieCard />
+        </div>
+      </div>
+
+      <div className="sidebar">
+        Sidebar will be here
+      </div>
+    </div>
+  </>
 );
