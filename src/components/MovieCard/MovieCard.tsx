93e50ac001c294b01a7779e1e9b2aa5e43b1b114
Sasha Skrinnik <sasha.skrinnik@gmail.com>
2022-07-06 10:05:15 +0300

    Fixed some mistakes

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 export const MovieCard: React.FC<Props> = ({ movie }) => (
-  <div className="card" key={movie.imgUrl}>
+  <div className="card">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
