a3c5fe4cd0c1ab2c72781c5b497a9ffd60a5b889
Kostiantyn <havryshkostya@gmail.com>
2022-07-06 11:42:19 +0400

    lets see

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,37 @@
+/* eslint-disable max-len */
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export interface Movies {
+  title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
+
+type Props = {
+  moviesList: Movies[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesList = [] }) => (
+  <>
+    <div className="movies">
+      {moviesList.map(movie => (
+        <div
+          key={movie.imdbId}
+          className="card"
+          data-cy="card"
+        >
+          <MovieCard
+            title={movie.title}
+            description={movie.description}
+            imgUrl={movie.imgUrl}
+            imdbUrl={movie.imdbUrl}
+          />
+        </div>
+      ))}
+    </div>
+  </>
 );
