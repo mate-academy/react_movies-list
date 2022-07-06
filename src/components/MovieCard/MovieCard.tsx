2f8179b8ec06ba96099ff6fd9871c0c09baa205d
Kate Khon <katerynakhon@gmail.com>
2022-07-06 12:11:30 +0300

    initial commit

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Card } from '../../types/Card';
 
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+interface Movie {
+  movie: Card
+}
+
+export const MovieCard: React.FC<Movie> = ({ movie }) => (
+  <div
+    className="card"
+    data-cy="card"
+    key={movie.imdbId}
+  >
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={movie.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">{movie.title}</p>
+        </div>
+      </div>
+
+      <div className="content">
+        {movie.description}
+        <br />
+        <a href={movie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Card } from '../../types/Card';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Movies = {
+  movies: Card[]
+};
+
+export const MoviesList: React.FC<Movies> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} />
+    ))}
+  </div>
 );
