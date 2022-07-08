a3653e0a365604ace8fbff00248713f704027ff9
Dima Pyvovarchuk <darkfix137@gmail.com>
2022-07-08 11:08:43 +0300

    props of movieCard were changed, key was added

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,12 +11,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard
-        title={movie.title}
-        description={movie.description}
-        imgUrl={movie.imgUrl}
-        imdbUrl={movie.imdbUrl}
-      />
+      <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
