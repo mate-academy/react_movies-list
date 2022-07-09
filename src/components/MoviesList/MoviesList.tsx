1539457be9f3c0a078759978889639083da4e27e
Artem Furdela <furdela.artem@gmail.com>
2022-07-09 15:48:39 +0300

    React movies first commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[],
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <>
+    <div className="movies" data-cy="card">
+      {movies.map(movie => (
+        <MovieCard
+          movie={movie}
+          key={movie.imdbId}
+        />
+      ))}
+    </div>
+  </>
 );
