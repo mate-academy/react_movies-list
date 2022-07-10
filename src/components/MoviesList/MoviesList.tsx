0126ecba94f0fa7714dd547501d318c56d9647f4
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-10 13:26:46 +0300

    Fixed

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -2,14 +2,6 @@ import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-interface Movie {
-  title: string;
-  description: string;
-  imgUrl: string;
-  imdbUrl: string;
-  imdbId: string;
-}
-
 type Props = {
   movies: Movie[];
 };
@@ -18,12 +10,7 @@ export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <div className="card" key={movie.imdbId}>
-        <MovieCard
-          title={movie.title}
-          description={movie.description}
-          imgUrl={movie.imgUrl}
-          imdbUrl={movie.imdbUrl}
-        />
+        <MovieCard movie={movie} />
       </div>
     ))}
   </div>
