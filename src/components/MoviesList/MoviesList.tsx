f8eddbb902c263378180ccb4c6a67a8974377bb3
Roman Nikitenko <Lomkaaa@ukr.net>
2022-07-19 07:46:50 +0300

    Task complete

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/movie';
+
+type Props = {
+  movie: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movie }) => (
+  <>
+    <div className="movies">
+      {movie.map(item => (
+        <MovieCard {...item} />
+      ))}
+    </div>
+  </>
 );
