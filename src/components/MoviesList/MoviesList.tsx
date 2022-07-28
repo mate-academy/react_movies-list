9d56fe36d2bba4c8f4408a2160e19c4ba196d69e
silchyn <ivan.silchin@gmail.com>
2022-07-28 14:04:05 +0300

    added movies list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
+/* eslint-disable max-len */
+
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => <MovieCard movie={movie} />)}
+  </div>
 );
