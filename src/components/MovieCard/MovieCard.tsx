7df01828d1341407d42f206b1525fcd85f2c8018
Nikita Biriukov <flimsy00@gmail.com>
2022-08-01 21:48:56 +0300

    key was added

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -6,9 +6,10 @@ import { Movie } from '../../types/movie';
 
 type Props = {
   movie: Movie,
+  key: string,
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => {
+export const MovieCard: React.FC<Props> = ({ movie, key }) => {
   const {
     title,
     description,
@@ -17,37 +18,39 @@ export const MovieCard: React.FC<Props> = ({ movie }) => {
   } = movie;
 
   return (
-    <>
-      <div className="card" data-cy="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img
-              src={imgUrl}
-              alt="Film logo"
-            />
-          </figure>
+    <div
+      className="card"
+      data-cy="card"
+      key={key}
+    >
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8">{title}</p>
+          </div>
         </div>
 
-        <div className="card-content">
-          <div className="media">
-            <div className="media-left">
-              <figure className="image is-48x48">
-                <img src="images/imdb-logo.jpeg" alt="imdb" />
-              </figure>
-            </div>
-
-            <div className="media-content">
-              <p className="title is-8">{title}</p>
-            </div>
-          </div>
-
-          <div className="content">
-            {description}
-            <br />
-            <a href={imdbUrl}>IMDB</a>
-          </div>
+        <div className="content">
+          {description}
+          <br />
+          <a href={imdbUrl}>IMDB</a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
