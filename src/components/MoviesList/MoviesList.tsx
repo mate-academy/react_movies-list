93083544d123de8348c1ae422824d49cdf67e183
Nikita Biriukov <flimsy00@gmail.com>
2022-08-01 21:00:30 +0300

    movie list was implemented

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/movie';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => <MovieCard movie={movie} />)}
+  </div>
 );
