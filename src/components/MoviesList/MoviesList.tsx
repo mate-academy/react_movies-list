f44f14c95629d99133ee1cc02ad2df5c5f23c284
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2022-08-03 12:34:12 +0300

    firstCommit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Movie } from '../../types/movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+
+  <div className="movies">
+    {movies.map(movie => (
+      <div
+        className="card"
+        data-cy="card"
+        key={movie.imdbId}
+      >
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
 );
