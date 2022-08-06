b62b6798f9817d1de494d7aa40b3bcd6cc04959e
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-08-06 13:25:58 +0300

    commit message

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface MovieCards {
+  title: string;
+  description: string;
+  imdbUrl: string;
+  imgUrl: string;
+}
+
+type Props = {
+  movies: MovieCards[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <ul>
+    <div className="movies">
+      {movies.map(movie => (
+        <li key={movie.title}>
+          <MovieCard {...movie} />
+        </li>
+      ))}
+    </div>
+  </ul>
 );
