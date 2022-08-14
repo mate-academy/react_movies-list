9b8fb6373f96c4162fe19f24cebf347d2748bf62
Sadovyi Denys <sadovyidenys9999@gmail.com>
2022-08-14 19:23:18 +0300

    Solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 import './MovieCard.scss';
+import { Movie } from '../../types/movie';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Movies = {
+  movie: Movie;
+};
+
+export const MovieCard: React.FC<Movies> = ({ movie }) => (
+  <div className="card" data-cy="card">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          src={movie.imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8">
+            {movie.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        {movie.description}
+        <br />
+        <a href={movie.imdbUrl}>IMDB</a>
+      </div>
+    </div>
+  </div>
 );
