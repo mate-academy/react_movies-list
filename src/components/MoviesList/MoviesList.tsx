595edbf4619260905c84e16a25e819fcffac0932
Oles Partyka <oles.partyka.27@gmail.com>
2022-09-06 07:31:15 +0200

    Solurion

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[],
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(item => <MovieCard movie={item} key={item.imdbId} />)}
+  </div>
 );
