52620219ef6521b713abecb0467c5bc3c089b169
Artemii Pavlenko <artemiipav@gmail.com>
2022-09-07 16:16:09 +0300

    React Movie List Crated

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+// import React from 'react';
+import { MovieTypes } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: MovieTypes[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie) => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </div>
 );
