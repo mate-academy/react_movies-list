bdd81da86e5c63c243bdfbed370160502215a6c7
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2022-09-07 20:03:11 +0300

    Movies List was created

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MovieType = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<MovieType> = ({ movies }) => (
+  <>
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  </>
 );
