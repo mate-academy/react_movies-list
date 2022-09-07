e1c4e5ebc46faea71605dc27b5cfcb19063e76b1
Max Kravchenko <max.kravchenko@gmail.com>
2022-09-07 12:52:51 +0300

    Add minor changes to pass gh tests

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
+    {movies.map(movieUnit => (
+      <MovieCard key={movieUnit.imdbId} movie={movieUnit} />
     ))}
   </div>
 );
