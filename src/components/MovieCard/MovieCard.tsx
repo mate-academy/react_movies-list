d0c893f2f647c6b61e26489dee195dbf52ed1a36
Sasha Kolomiiets <kinbean444@gmail.com>
2022-09-08 13:08:44 +0300

    added destructurization

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -5,12 +5,19 @@ type Props = {
   movie: Movie;
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
+export const MovieCard: React.FC<Props> = ({
+  movie: {
+    imgUrl,
+    title,
+    description,
+    imdbUrl,
+  },
+}) => (
   <div className="card" data-cy="card">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
-          src={movie.imgUrl}
+          src={imgUrl}
           alt="Film logo"
         />
       </figure>
@@ -25,14 +32,14 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
         </div>
 
         <div className="media-content">
-          <p className="title is-8">{movie.title}</p>
+          <p className="title is-8">{title}</p>
         </div>
       </div>
 
       <div className="content">
-        {movie.description}
+        {description}
         <br />
-        <a href={movie.imdbUrl}>IMDB</a>
+        <a href={imdbUrl}>IMDB</a>
       </div>
     </div>
   </div>
