da52ee58899056dbfbe20e120adaa71328be3226
Jamal Amoian <leonidagutin09@gmail.com>
2022-09-08 12:55:59 +0300

    use destructuring in MovieCard file and deleted unnecessary wrapper in MovieList

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -7,7 +7,14 @@ type Props = {
   movie: Movie;
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
+export const MovieCard: React.FC<Props> = ({
+  movie: {
+    imgUrl,
+    title,
+    description,
+    imdbUrl,
+  },
+}) => (
   <>
     <div className="card" data-cy="Movie">
       <div className="card-image">
@@ -15,7 +22,7 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
           <img
             data-cy="MovieImage"
             // eslint-disable-next-line max-len
-            src={movie.imgUrl}
+            src={imgUrl}
             alt="Film logo"
           />
         </figure>
@@ -31,18 +38,18 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
+              {title}
             </p>
           </div>
         </div>
 
         <div className="content">
           <p data-cy="MovieDescription">
-            {movie.description}
+            {description}
           </p>
 
           <a
-            href={movie.imdbUrl}
+            href={imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
