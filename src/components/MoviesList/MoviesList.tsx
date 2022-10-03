a50e7daff889e0987b9d802da773798cd5d21497
Serhii Vovk <gear.fq@gmail.com>
2022-10-03 11:41:12 +0300

    Add sematic tag

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -8,11 +8,11 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
+  <ul className="movies">
     {movies.map(movie => (
-      <div className="card" data-cy="Movie" key={movie.imdbId}>
+      <li className="card" data-cy="Movie" key={movie.imdbId}>
         <MovieCard movie={movie} />
-      </div>
+      </li>
     ))}
-  </div>
+  </ul>
 );
