7669be0e2f6faf31b50866562c751959e36f0048
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-10-10 20:57:40 +0300

    creating movies cards

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  moviesFromServer: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ moviesFromServer }) => (
+  <div className="movies">
+    {moviesFromServer.map((movie: Movie) => (
+      <MovieCard
+        key={movie.imdbId}
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+      />
+    ))}
+  </div>
 );
