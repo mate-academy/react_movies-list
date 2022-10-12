58488a7162fe939f07ce5c5eafc5302f878c1702
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-10-12 16:24:31 +0300

    Implement rendering movies list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
+interface Props {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <>
+    {movies.map((movie) => (
+      <div className="card" data-cy="Movie" key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </div>
+    ))}
+  </>
 );
