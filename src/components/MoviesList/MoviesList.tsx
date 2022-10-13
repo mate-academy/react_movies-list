e642cf105f6bf0e659ff5cebe6151ef5f0a0098c
Vladimir Dyblia <vladimirdublya@gmail.com>
2022-10-13 16:24:42 +0300

    added symantics

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -8,13 +8,11 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <div className="movies">
-    {movies.map((movie: Movie) => (
-      <>
-        <MovieCard
-          movie={movie}
-        />
-      </>
+  <ul className="movies">
+    {movies.map(movie => (
+      <li key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
     ))}
-  </div>
+  </ul>
 );
