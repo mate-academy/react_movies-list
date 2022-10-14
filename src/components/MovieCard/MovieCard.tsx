8760ce045b20689bea25f949e63f2c9dd26e284e
Iryna Borniak <la.aerea.1@gmail.com>
2022-10-15 00:55:27 +0300

    refactor: add type for movie

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,19 +1,18 @@
 import './MovieCard.scss';
 import { Movie } from '../../types/Movie';
 
-export const MovieCard: React.FC<Movie> = ({
-  title,
-  description,
-  imgUrl,
-  imdbUrl,
-}) => (
+type Props = {
+  movie: Movie;
+};
+
+export const MovieCard: React.FC<Props> = ({ movie }) => (
   <>
     <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img
             data-cy="MovieImage"
-            src={imgUrl}
+            src={movie.imgUrl}
             alt="Film logo"
           />
         </figure>
@@ -29,18 +28,18 @@ export const MovieCard: React.FC<Movie> = ({
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {title}
+              {movie.title}
             </p>
           </div>
         </div>
 
         <div className="content">
           <p data-cy="MovieDescription">
-            {description}
+            {movie.description}
           </p>
 
           <a
-            href={imdbUrl}
+            href={movie.imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
