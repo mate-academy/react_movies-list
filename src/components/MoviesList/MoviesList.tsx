b9590806f9cc4d9bdca5a3a41401c7385f61c4a2
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-10-20 16:22:14 +0300

    movies_list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    <ul className="movies">
+      {movies.map((movie: Movie) => (
+        <li
+          className="card"
+          data-cy="Movie"
+          key={movie.imdbId}
+        >
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  </>
 );
--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,5 +1,9 @@
+import { Url } from 'url';
+
 export interface Movie {
   title: string;
-
-  // add all the other field
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
 }
