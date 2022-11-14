eaf1600ade6415b96c91cb86b71bb2a06fb392a4
Eugene P <eugenep@MacBook-Pro.local>
2022-11-14 22:27:53 +0200

    movieslist

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,5 +1,11 @@
 export interface Movie {
   title: string;
+  description: string;
+  imgUrl: string;
+  imdbUrl: string;
+  imdbId: string;
+}
 
-  // add all the other field
+export interface Props {
+  moviesFromServer: Movie[]
 }
