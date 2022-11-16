22f750f7b5bcaa24194e45b0d9426f5439945d72
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2022-11-16 22:50:34 +0200

    after second review

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -8,9 +8,9 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <section className="movies">
+  <ul className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
-  </section>
+  </ul>
 );
