c2dc24dda519f658d3d59de7eb950ed090d66d3c
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-11-16 19:08:52 +0200

    v02 fixes due to checklist

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -10,6 +10,8 @@ type Props = {
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
-    {movies.map((movie) => <MovieCard key={movie.imdbId} movie={movie} />)}
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
   </div>
 );
