a16afb0ce1f37d4cd2d32e0058a70bbc67040760
Zaika Andrii <anzaika13@gmail.com>
2022-11-17 19:45:42 +0200

    Movies list is fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MoviesList moviesFromServer={moviesFromServer} />
-      </div>
-
-      <div className="sidebar" data-cy="Sidebar">
-        Sidebar will be here
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MoviesList moviesFromServer={moviesFromServer} />
     </div>
-  </>
+
+    <div className="sidebar" data-cy="Sidebar">
+      Sidebar will be here
+    </div>
+  </div>
 );
