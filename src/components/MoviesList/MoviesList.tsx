183c3a6ff84c9a75fc74c6375f928f65834c9f50
Yaro Bey <eltrece1@gmail.com>
2022-11-23 00:38:23 +0300

    fixed code

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -9,11 +9,9 @@ type Props = {
 };
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <>
-    <div className="movies">
-      {movies.map(film => (
-        <MovieCard key={film.imdbId} movie={film} />
-      ))}
-    </div>
-  </>
+  <div className="movies">
+    {movies.map(film => (
+      <MovieCard key={film.imdbId} movie={film} />
+    ))}
+  </div>
 );
