3f6c1c66053863c1f71c4e5d88d2f40ea0a0e1f7
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-12-01 17:20:06 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-/* eslint-disable max-len */
 import React from 'react';
 
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
-// import { Movie } from './types/Movie';
-
-// type Movies = {
-//   props: Movie[]
-// };
 
 export const App: React.FC = () => (
   <div className="page">
