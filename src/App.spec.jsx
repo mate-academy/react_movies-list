90b10a480df081cb8d5b5e8eafac87c41cb99df2
Margarita Chorniak <chorniak.rita@gmail.com>
2022-12-09 14:04:26 +0200

    solution

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { mount } from '@cypress/react';
-// import { App } from './App';
+import React from 'react';
+import { mount } from '@cypress/react';
+import { App } from './App';
 
-// describe('App', () => {
-//   beforeEach(() => {
-//     mount(<App />);
-//   });
+describe('App', () => {
+  beforeEach(() => {
+    mount(<App />);
+  });
 
-//   it('should render all the movies', () => {
-//     cy.getByDataCy('Movie').should('have.length', 5);
-//   });
+  it('should render all the movies', () => {
+    cy.getByDataCy('Movie').should('have.length', 5);
+  });
 
-//   it('should render a sidebar', () => {
-//     cy.getByDataCy('Sidebar')
-//       .should('have.text', 'Sidebar will be here');
-//   });
-// });
+  it('should render a sidebar', () => {
+    cy.getByDataCy('Sidebar')
+      .should('have.text', 'Sidebar will be here');
+  });
+});
