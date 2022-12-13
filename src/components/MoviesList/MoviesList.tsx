05580c7520ac6a28ce0f601c63c8f4577cbe9f6a
s3rhiienko <r.i.serhiienko@gmail.com>
2022-12-13 22:56:30 +0200

    Movies List

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
+import { Movies } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+export const MoviesList: React.FC<Movies> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
