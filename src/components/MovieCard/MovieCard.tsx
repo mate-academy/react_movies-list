8f9394d633f977beae02a9e417db493246ce4630
Anton Martyniuk <antonmartyniuk@gmail.com>
2022-12-17 19:37:46 +0200

    Solution react_movies_list

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -22,47 +22,13 @@ export const MovieCard: React.FC<Props> = ({
   <>
     <CardImg imgUrl={imgUrl} />
 
-    {/* <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div> */}
-
     <div className="card-content">
       <Media title={title} />
-      {/* <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
-          </figure>
-        </div>
 
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            {title}
-          </p>
-        </div>
-      </div> */}
       <Content
         description={description}
         imdbUrl={imdbUrl}
       />
-      {/* <div className="content">
-        <p data-cy="MovieDescription">
-          {description}
-        </p>
-
-        <a
-          href={imdbUrl}
-          data-cy="MovieLink"
-        >
-          IMDB
-        </a>
-      </div> */}
     </div>
   </>
 );
