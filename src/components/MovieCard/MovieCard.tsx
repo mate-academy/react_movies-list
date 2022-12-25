bf63a29d05fa306461a37cf48309b5b3736d57f1
Anatolii <batstolya@gmail.com>
2022-12-25 23:25:28 +0200

    small fix

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -17,11 +17,20 @@ export const MovieCard: React.FC<Props> = ({ movie }) => {
 
   return (
     <div className="card" data-cy="Movie">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            data-cy="MovieImage"
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
       <div className="card-content">
         <div className="media">
           <div className="media-left">
             <figure className="image is-48x48">
-              <img src={imgUrl} alt="imdb" data-cy="MovieImage" />
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
             </figure>
           </div>
 
