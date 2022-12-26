43e674527ea16a4950d400e611f70bc19fc61458
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2022-12-26 19:57:40 +0100

    Movies list created

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MovieListProps = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<MovieListProps> = ({ movies }) => (
+  <>
+    <ul>
+      { movies.map(movie => {
+        return (
+          <li key={movie.imdbId}>
+            <MovieCard movie={movie} />
+          </li>
+        );
+      })}
+    </ul>
+  </>
 );
