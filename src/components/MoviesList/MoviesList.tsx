1a8eb0a7b496969d9472fb6eaf76424ce2b40d6c
Nazarii Kyselevych <nazarkysel@gmail.com>
2022-12-27 22:17:28 +0200

    created MovieCard and MoviesList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Prop = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Prop> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)}
+  </div>
 );
