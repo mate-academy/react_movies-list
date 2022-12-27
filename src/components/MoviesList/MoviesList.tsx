f4b4a37096c65cf28f878134bb81cb45f41c198e
illia_kots <illyahennadievich@gmail.com>
2022-12-27 14:37:23 +0200

    React_Movies-List has been complited

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <ul className="movies">
+    {movies.map(movie => (
+      <li
+        className="card"
+        data-cy="card"
+        key={movie.imdbId}
+      >
+        <MovieCard
+          movie={movie}
+        />
+      </li>
+      ))}
+  </ul>
 );
