81de0dd631a8c3fe7d5ec8346f8c3a2e511a548a
VladimirEdelmann <VladimirEdelmann@gmail.com>
2022-12-28 04:31:38 +0200

    Add unique key

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,8 +11,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {
-      movies.map(movie => <MovieCard movie={movie} />)
+      movies.map(movie => <MovieCard movie={movie} key={movie.imdbId} />)
     }
   </div>
-
 );
