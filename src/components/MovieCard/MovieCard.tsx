ca0736cc4329adbbfe080e0420a58fefaf1c19a4
Roman Korchovskyi <korchovskyi.roman@gmail.com>
2022-12-29 21:16:50 +0200

    fixed, added destructuring

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -8,14 +8,21 @@ type MovieCardProps = {
 };
 
 export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+  const {
+    title,
+    description,
+    imgUrl,
+    imdbUrl,
+  } = movie;
+
   return (
     <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img
             data-cy="MovieImage"
-            src={movie.imgUrl}
-            alt={movie.title}
+            src={imgUrl}
+            alt={title}
           />
         </figure>
       </div>
@@ -33,16 +40,16 @@ export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
+              {title}
             </p>
           </div>
         </div>
 
         <div className="content">
-          <p data-cy="MovieDescription">{movie.description}</p>
+          <p data-cy="MovieDescription">{description}</p>
 
           <a
-            href={movie.imdbUrl}
+            href={imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
