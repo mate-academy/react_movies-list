a083a47e8966c982b004e4ab354262f626ced0c4
Katya Suvorova <katerinasuvorova97@gmail.com>
2023-01-16 14:22:14 +0200

    Fixed after code review

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -7,19 +7,26 @@ type Props = {
   movie: Movie;
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
-  <>
-    <div className="card" data-cy="Movie">
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+  const {
+    imgUrl,
+    title,
+    description,
+    imdbUrl,
+  } = movie;
+
+  return (
+    <li className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img
             data-cy="MovieImage"
-            src={movie.imgUrl}
+            src={imgUrl}
             alt="Film logo"
           />
         </figure>
       </div>
-
+  
       <div className="card-content">
         <div className="media">
           <div className="media-left">
@@ -27,27 +34,27 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
               <img src="images/imdb-logo.jpeg" alt="imdb" />
             </figure>
           </div>
-
+  
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
+              {title}
             </p>
           </div>
         </div>
-
+  
         <div className="content">
           <p data-cy="MovieDescription">
-            {movie.description}
+            {description}
           </p>
-
+  
           <a
-            href={movie.imdbUrl}
+            href={imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
           </a>
         </div>
       </div>
-    </div>
-  </>
-);
+    </li>
+  );
+}
