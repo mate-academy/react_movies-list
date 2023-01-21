b5119d43b7332787c1f49d0414a3bf0bd0e03bee
miamarichka <ilchyshyn.marichka@gmail.com>
2023-01-21 09:55:44 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ import { MoviesList } from './components/MoviesList/MoviesList';
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
-      <div className="movies">
-        <MoviesList movies={moviesFromServer} />
-      </div>
+      <MoviesList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
