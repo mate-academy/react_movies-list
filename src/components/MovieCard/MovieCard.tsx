1c9c0dcc54c0c6e445d7b04482afbb477c353eaf
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-01-22 15:47:40 +0200

    icon path fixes

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -22,7 +22,7 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="/images/imdb-logo.jpeg" alt="imdb" />
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
           </figure>
         </div>
 
