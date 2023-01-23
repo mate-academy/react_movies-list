15398fe22be7fb87b76e399934e3e4af6d3f9125
Ivan Vyhinnyi <ivan665303@gmail.com>
2023-01-23 21:26:38 +0200

    Solution 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,8 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
-import { Movie } from './types/Movie';
 
-type Props = {
-  movies: Movie[];
-};
-
-export const App: React.FC<Props> = () => (
+export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
       <MoviesList movies={moviesFromServer} />
