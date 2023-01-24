1add8d852c1ff39b22f7262f545bd1a2d2047bcf
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-24 21:23:19 +0200

    completed

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" id={movie.imdbId} data-cy="Movie">
+        <MovieCard movie={movie} />
+      </div>
+    ))}
+  </div>
 );
