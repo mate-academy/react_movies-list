13b8c29e9a8e32a65bfe0440a6a99bbf76dcb07a
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-01-24 21:28:09 +0200

    Fixed stylse and props

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,10 +11,7 @@ export const MoviesList: React.FC<Props> = ({ movies }) => {
   return (
     <div className="movies">
       {movies.map(movie => (
-        <MovieCard
-          key={movie.imdbId}
-          movie={movie}
-        />
+        <MovieCard key={movie.imdbId} movie={movie} />
       ))}
     </div>
   );
