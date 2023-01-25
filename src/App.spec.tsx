f614dddeb69007a3489fe572984f953f7b9acca0
Dima Korol <korovaidima@gmail.com>
2023-01-25 20:54:14 +0100

    8

---
+++ src/App.spec.tsx
@@ -0,0 +1,17 @@
+import { mount } from '@cypress/react';
+import { App } from './App';
+
+describe('App', () => {
+  beforeEach(() => {
+    mount(<App />);
+  });
+
+  it('should render all the movies', () => {
+    cy.getByDataCy('Movie').should('have.length', 5);
+  });
+
+  it('should render a sidebar', () => {
+    cy.getByDataCy('Sidebar')
+      .should('have.text', 'Sidebar will be here');
+  });
+});
