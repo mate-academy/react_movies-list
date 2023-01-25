7b3eb29580a6f88d35aab0a244ea7b66577c5227
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2023-01-25 12:52:18 +0200

    1st commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MoviesArr = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<MoviesArr> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        title={movie.title}
+        description={movie.description}
+        imgUrl={movie.imgUrl}
+        imdbUrl={movie.imdbUrl}
+        imdbId={movie.imdbId}
+        key={movie.imdbId}
+      />
+    ))}
+  </div>
 );
