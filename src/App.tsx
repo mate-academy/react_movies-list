54012cb9b5f2e8882edd2c8688353168d2610693
Dima Korol <korovaidima@gmail.com>
2023-01-26 10:06:23 +0100

    5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable max-len */
 import React from 'react';
-
+import { MoviesList } from './components/MoviesList';
 import './App.scss';
 
-import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => (
