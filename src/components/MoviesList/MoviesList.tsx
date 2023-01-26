03783436f25f6a418ae69d09e9521f932da92fa6
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-01-26 15:27:23 +0200

    deleted unnecessary default prop

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -7,7 +7,7 @@ type Props = {
   movies: Movie[];
 };
 
-export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map((movie) => (
       <MovieCard movie={movie} key={movie.imdbId} />
