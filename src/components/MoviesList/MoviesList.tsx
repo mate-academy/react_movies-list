253afaa349f8c07b7894a65665b9400d505df7c7
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-03 15:13:49 +0200

    Add ul in MoviesList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,11 +11,13 @@ export const MoviesList: React.FC<Props> = ({ movies }) => {
   const content
   = movies.length > 0 ? (
     <div className="movies">
-      <MovieCard movie={movies[0]} />
-      <MovieCard movie={movies[1]} />
-      <MovieCard movie={movies[2]} />
-      <MovieCard movie={movies[3]} />
-      <MovieCard movie={movies[4]} />
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.imdbId}>
+            <MovieCard movie={movie} />
+          </li>
+        ))}
+      </ul>
     </div>
   ) : (
     <div className="movies" />
