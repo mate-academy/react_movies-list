34ad2bf442412d1e489569e035a34edd1194e59f
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-02-03 12:32:44 +0200

    Finish task

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => {
+  const content
+  = movies.length > 0 ? (
+    <div className="movies">
+      <MovieCard movie={movies[0]} />
+      <MovieCard movie={movies[1]} />
+      <MovieCard movie={movies[2]} />
+      <MovieCard movie={movies[3]} />
+      <MovieCard movie={movies[4]} />
+    </div>
+  ) : (
+    <div className="movies" />
+  );
+
+  return content;
+};
