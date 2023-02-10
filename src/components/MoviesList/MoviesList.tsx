a7c1f2bfddad5bfb312d16ab1d9d5d76fcee9442
Nick Busarhin <busarginkolia@gmail.com>
2023-02-10 11:11:39 +0200

    filmList

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie []
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    {movies.map(movie => (
+      <div className="card" data-cy="Movie" key={movie.imdbId}>
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </>
 );
