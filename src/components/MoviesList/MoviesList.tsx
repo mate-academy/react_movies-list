21fd959e64f42cb0ee9834458f6f8331d4de0af6
Sergey Kovyazin <mr.roland1989@gmail.com>
2023-02-18 01:55:04 +0200

    Solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Props } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <>
+    <ul className="movies">
+      {movies.map((movie) => (
+        <li>
+          <MovieCard {...movie} key={movie.imdbId} />
+        </li>
+      ))}
+    </ul>
+  </>
 );
