b57b9f32e4eb8e7143fee89ba112d51ab1b1c5f0
Sergey Kovyazin <mr.roland1989@gmail.com>
2023-02-22 21:44:04 +0200

    fixes

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,18 +3,16 @@ import { Movie } from '../../types/Movie';
 import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export type Props = {
-  movies?: Movie[]
+type Props = {
+  movies: Movie[] | []
 };
 
 export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
-  <>
-    <ul className="movies">
-      {movies.map((movie) => (
-        <li>
-          <MovieCard movie={movie} key={movie.imdbId} />
-        </li>
-      ))}
-    </ul>
-  </>
+  <ul className="movies">
+    {movies.length && movies.map((movie) => (
+      <li key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
+    ))}
+  </ul>
 );
