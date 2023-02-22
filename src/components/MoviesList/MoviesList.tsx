ed4cd09640bab5ded037f528473cd8510588b846
Sofiia Triasko <sofia13tryasko@gmail.com>
2023-02-22 18:55:42 +0200

    final commit

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -10,7 +10,9 @@ type MovieType = {
 export const MoviesList: React.FC <MovieType> = ({ movies }) => (
   <ul className="movies">
     {movies.map((movie) => (
-      <MovieCard movie={movie} />
+      <li className="card" key={movie.imdbId}>
+        <MovieCard movie={movie} />
+      </li>
     ))}
   </ul>
 );
