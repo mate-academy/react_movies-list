53f8a8e73c4ef795577b9f6872d13c0ea8265c5d
Sergey Vorobey <sergey88nik@gmail.com>
2023-02-28 22:15:11 +0200

    React movie list

--- src/components/MoviesList/MoviesList.spec.jsx
+++ src/components/MoviesList/MoviesList.spec.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-filename-extension */
+// /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { mount } from '@cypress/react';
 import { MoviesList } from './MoviesList';
