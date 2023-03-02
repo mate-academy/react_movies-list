13ce61c7045a509cb4f4794c7ac1d4ec0fd82fd4
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-03-02 16:00:14 +0100

    made something with types sol 1.2

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
-
-interface Movie {
-  title: string;
-  description: string;
-  imgUrl: string;
-  imdbUrl: string;
-  imdbId: string;
-}
+import { Movie } from '../../types/Movie';
 
 type Props = {
   movies: Movie[];
@@ -17,7 +10,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard {...movie} key={movie.imdbId} />
+      <MovieCard movie={movie} />
     ))}
   </div>
 );
