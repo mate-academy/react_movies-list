4e851ab217e90e75182f1ca5278a4ac41649d1e3
timoxa2t <alex2t172@gmail.com>
2023-03-13 16:50:57 +0200

    corrections

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -7,54 +7,56 @@ type Props = {
   movie: Movie,
 };
 
-export const MovieCard: React.FC<Props> = (
-  {
-    movie: {
-      description,
-      imdbUrl,
-      title,
-      imgUrl,
-    },
-  },
-) => (
-  <div className="card" data-cy="Movie">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div>
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+  const {
+    description,
+    imdbUrl,
+    imgUrl,
+    title,
+  } = movie;
 
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
-          </figure>
+  return (
+    <div className="card" data-cy="Movie">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            data-cy="MovieImage"
+            src={imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8" data-cy="MovieTitle">
+              {title}
+            </p>
+          </div>
         </div>
 
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            {title}
+        <div className="content">
+          <p data-cy="MovieDescription">
+            {description}
           </p>
+
+          <a
+            href={imdbUrl}
+            data-cy="MovieLink"
+            rel="noreferrer"
+            target="_blank"
+          >
+            IMDB
+          </a>
         </div>
       </div>
-
-      <div className="content">
-        <p data-cy="MovieDescription">
-          {description}
-        </p>
-
-        <a
-          href={imdbUrl}
-          data-cy="MovieLink"
-        >
-          IMDB
-        </a>
-      </div>
     </div>
-  </div>
-);
+  );
+};
