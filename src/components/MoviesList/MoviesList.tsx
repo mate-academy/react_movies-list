339d49d2d80ed6e4b650866d194bd77c798bafc3
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-03-13 14:12:53 +0200

    add target ro link and fix long line

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -9,6 +9,11 @@ type Props = {
 
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
-    {movies.map((movie) => <MovieCard key={movie.imdbId} movie={movie} />)}
+    {movies.map((movie) => (
+      <MovieCard
+        key={movie.imdbId}
+        movie={movie}
+      />
+    ))}
   </div>
 );
