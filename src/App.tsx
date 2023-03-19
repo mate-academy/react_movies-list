6eefe145e0d49b689bcba087d26c2b1f4990ac87
Peti51 <galpeter31@gmail.com>
2023-03-19 11:12:00 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 import './App.scss';
-import { MoviesList } from './components/MoviesList/MoviesList';
-import moviesFromServer from './api/movies.json';
+// import { MoviesList } from './components/MoviesList/MoviesList';
+// import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
-      <MoviesList movie={moviesFromServer} />
+      {/* <MoviesList movie={moviesFromServer} /> */}
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
