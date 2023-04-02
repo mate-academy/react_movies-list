26d722ddc212118a4733c6a7641ecaa57ebcbacb
Nazar <nazar378378379379@gmail.com>
2023-04-02 18:37:46 +0300

    develop

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    <ul>
+      {movies.map(movie => (
+        <li key={movie.imdbId}>
+          <MovieCard movie={movie} key={movie.imdbId} />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
