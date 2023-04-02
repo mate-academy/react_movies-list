fe01ec2972c1620d2812a824e0fa48b9d98d066e
Serhii Nikolin <sergeynikolin@gmail.com>
2023-04-02 15:26:52 +0300

    Ready

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movieList: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movieList }) => (
+  <ul className="movies">
+    {movieList.map(item => (
+      <li key={item.imdbId}>
+        <MovieCard {...item} />
+      </li>
+    ))}
+  </ul>
 );
