06a4be867e68a4eb5df79584de25bbc3ec0daa46
EhorSaprykin <astralik1993@gmail.com>
2023-04-03 10:57:31 +0300

    List done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,14 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './MoviesList.scss';
+import { MovieList } from '../../types/MovieList';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+
+export const MoviesList: React.FC<MovieList> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} />
+    ))}
+  </div>
 );
