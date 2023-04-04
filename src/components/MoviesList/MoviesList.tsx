c16b3b948e02da548e69cbd6348a1554bcfa68f5
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-04-04 12:47:11 +0200

    task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+export const MoviesList: React.FC = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie) => (
+      <MovieCard movie={movie} />
+
+    ))}
+  </div>
 );
