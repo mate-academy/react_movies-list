439467ae0698d8d9b9f3761a6015ead4a7edb73c
Ihor <ikruzhynskyy@gmail.com>
2023-04-07 22:49:00 +0300

    created movies app

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { MovieListProps } from '../../types/MovieListProps';
+import { MovieCard } from '../MovieCard';
+
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+export const MoviesList: React.FC<MovieListProps> = ({ movies }) => {
+  const key = uuidv4();
+
+  return (
+    <div className="movies">
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={key} />
+      ))}
+    </div>
+  );
+};
