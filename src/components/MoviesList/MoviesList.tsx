bb239e4c10fc8d40ce83604ce674a524051d1b5e
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-04-07 23:00:13 +0300

    made movies list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './MoviesList.scss';
+// import { type } from 'os';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
