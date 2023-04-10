66f2913873b24af2534bc91f66957629d1dc4b4e
Denys Krysanov <denis.krisanov321@gmail.com>
2023-04-10 16:10:16 +0300

    movies render list added

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MoviesListProps = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<MoviesListProps> = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie) => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
