6ae9d680618b79ff3faf5afd12b9aac4cb538bb2
glots-a <glots@ukr.net>
2023-04-11 14:38:17 +0300

    movies-list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+interface PropsList {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<PropsList> = ({ movies }) => (
+  <div className="movies">
+    {
+      movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))
+    }
+  </div>
 );
