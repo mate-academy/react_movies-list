c4ff4b8cc5834491977944949adfa6453ba81ee2
Hamuud <artemlask2014@gmail.com>
2023-04-11 19:28:06 +0300

    Create Movie list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+import { MovieListProps } from '../../types/MovieListProps';
+import { MovieCards } from '../MovieCard';
+
+export const MoviesList: React.FC<MovieListProps> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map((movie) => (
+        <MovieCards movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
