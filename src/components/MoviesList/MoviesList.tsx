c38677e0a18b24f72801672a2fdefd8e95bc7f57
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-04-11 21:08:47 +0200

    fix

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Movies = {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Movies> = ({ movies }) => (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
 );
