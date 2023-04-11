617d6306ee79aa81aa129ee8905087bb1358f5b8
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-04-11 23:59:19 +0300

    Solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,6 +2,59 @@ import React from 'react';
 
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Props = {
+  title: string;
+  imgUrl: string;
+  imdbUrl: string;
+  description: string;
+};
+
+export const MovieCard: React.FC<Props> = (
+  {
+    title,
+    imgUrl,
+    imdbUrl,
+    description,
+  },
+) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={imgUrl}
+          alt={title}
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src={imdbUrl} alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">
+          {description}
+        </p>
+
+        <a
+          href="https://www.imdb.com/title/tt1375666"
+          data-cy="MovieLink"
+        >
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
+import { Movie } from '../../types/Movie';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <ul>
+    {movies.map(movie => (
+      <li key={movie.imdbId}>
+        <MovieCard
+          title={movie.title}
+          imgUrl={movie.imgUrl}
+          imdbUrl={movie.imdbUrl}
+          description={movie.description}
+        />
+      </li>
+    ))}
+  </ul>
 );
