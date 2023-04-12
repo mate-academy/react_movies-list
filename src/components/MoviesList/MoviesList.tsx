4c3569565f6787a2208047943e1bb8a3cb5cc875
Alisa Vinnyk <alisavinnyk@gmail.com>
2023-04-12 15:48:21 +0100

    implemented MoviesList component

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type MoviesListProps = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<MoviesListProps> = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie) => (
+      <MovieCard movie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
