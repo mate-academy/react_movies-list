d0e07858fa9d6539600c4e2529ff74c5fdffe268
Valery Sazonov <vksazonov@gmail.com>
2023-04-12 12:28:36 +0300

    Changed due to remarks

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './MovieCard.scss';
 import { Movie } from '../../types/Movie';
 
@@ -7,45 +6,58 @@ type Props = {
   movie: Movie
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
-  <>
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img
-          data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt="Film logo"
-        />
-      </figure>
-    </div>
+export const MovieCard: React.FC<Props> = ({ movie }) => {
+  const {
+    imgUrl,
+    title,
+    description,
+    imdbUrl,
+    imdbId,
+  } = movie;
 
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
+  return (
+    <>
+      <div
+        className="card"
+        data-cy="Movie"
+        key={imdbId}
+      >
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img
+              data-cy="MovieImage"
+              src={imgUrl}
+              alt="Film logo"
+            />
           </figure>
         </div>
 
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            {movie.title}
-          </p>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-48x48">
+                <img src="images/imdb-logo.jpeg" alt="imdb" />
+              </figure>
+            </div>
+
+            <div className="media-content">
+              <p className="title is-8" data-cy="MovieTitle">
+                {title}
+              </p>
+            </div>
+          </div>
+
+          <div className="content">
+            <p data-cy="MovieDescription">
+              {description}
+            </p>
+
+            <a href={imdbUrl} data-cy="MovieLink">
+              IMDB
+            </a>
+          </div>
         </div>
       </div>
-
-      <div className="content">
-        <p data-cy="MovieDescription">
-          {movie.description}
-        </p>
-
-        <a
-          href={movie.imdbUrl}
-          data-cy="MovieLink"
-        >
-          IMDB
-        </a>
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
