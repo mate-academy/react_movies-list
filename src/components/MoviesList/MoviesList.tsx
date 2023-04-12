cede33e9e4ed9d0cefff8b77cf138f3d787521cb
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-04-12 09:52:25 +0300

    rendered movies list

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie[]
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map((movie) => {
+      return (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      );
+    })}
+  </div>
 );
