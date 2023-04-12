cac98457f609837326b6cc3a6ea9203b44632cf8
Valery Sazonov <vksazonov@gmail.com>
2023-04-12 14:39:42 +0300

    Added key mo MovieCard

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -15,10 +15,7 @@ export const MovieCard: React.FC<Props> = ({ movie }) => {
   } = movie;
 
   return (
-    <div
-      className="card"
-      data-cy="Movie"
-    >
+    <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
           <img
