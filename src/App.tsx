ab535e473570663ff45b59646d6ca6aa0833893c
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-04-23 17:05:44 +0300

    removed props type from App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
-import { Movie } from './types/Movie';
 
-type Props = {
-  movie?: Movie[];
-};
-
-export const App: React.FC<Props> = () => (
+export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
       <MoviesList movies={moviesFromServer} />
