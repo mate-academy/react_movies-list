d11efc6cffcb1f3272fdaaa7116e055b8e6ec10b
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-04-27 14:45:40 +0200

    solution4

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,11 +3,11 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
 
-type TypeListProps = {
+interface MovieListProps {
   movies: Movie[];
 };
 
-export const MoviesList: React.FC<TypeListProps> = ({ movies }) => (
+export const MoviesList: React.FC<MovieListProps> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
