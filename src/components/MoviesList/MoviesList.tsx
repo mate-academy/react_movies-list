7023e471e85456fa65800b4fba5bc9613729ac6a
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-02 10:50:38 +0300

    commit message

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  movies: Movie [];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div className="card" key={movie.imdbId} data-cy="Movie">
+        <MovieCard {...movie} />
+      </div>
+    ))}
+  </div>
 );
