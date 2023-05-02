4c0aeb2ea1c080e7d098d206de9026c8cf6af964
Nikitin Evgeny <ngenya81@gmail.com>
2023-05-02 11:22:29 +0300

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
-import { Movie } from './types/Movie';
 
-type Props = {
-  movies: Movie [];
-};
-
-export const App: React.FC<Props> = () => (
+export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
       <MoviesList movies={moviesFromServer} />
@@ -20,4 +15,3 @@ export const App: React.FC<Props> = () => (
   </div>
 );
 export default App;
-//
