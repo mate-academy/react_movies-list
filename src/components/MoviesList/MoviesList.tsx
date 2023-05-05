5680b152f48ec6a31cba20a45047c81f6b05eaae
sylcym <prusak.syl@gmail.com>
2023-05-05 16:46:29 +0200

    task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,11 +3,11 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
 
-type Props = {
+interface MovieListProps {
   movies: Movie[];
-};
+}
 
-export const MoviesList: React.FC<Props> = ({ movies }) => {
+export const MoviesList: React.FC<MovieListProps > = ({ movies }) => {
   return (
     <ul className="movies">
       {
