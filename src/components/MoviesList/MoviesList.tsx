9123e717f5c2271b6cf1c40c17fa93b2d4f8797f
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-05-05 14:00:17 +0200

    fix

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,11 +3,11 @@ import './MoviesList.scss';
 import { Movie } from '../../types/Movie';
 import { MovieCard } from '../MovieCard';
 
-type Props = {
+interface MovieListProps {
   movies: Movie[];
-};
+}
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
+export const MoviesList: React.FC<MovieListProps> = ({ movies }) => (
   <div className="movies">
     {movies.map((movie) => (
       <MovieCard movie={movie} key={movie.imdbId} />
