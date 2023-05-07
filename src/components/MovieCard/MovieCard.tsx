53996f40cd03a486c8490236ae2ff3d99ff9a3ee
Erik Karaman <erikkaraman7@gmail.com>
2023-05-07 23:14:34 +0300

    add iterface Movie

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,49 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 import './MovieCard.scss';
 
 export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            Inception
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">
+          Follows the lives of eight very different couples in dealing with
+          their love lives in various loosely interrelated tales all set
+          during a frantic month before Christmas in London, England.
+        </p>
+
+        <a
+          href="https://www.imdb.com/title/tt1375666"
+          data-cy="MovieLink"
+        >
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
 );
