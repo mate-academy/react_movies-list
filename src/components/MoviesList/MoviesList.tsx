5565c3e4f4ca2a49e35cfc8aef85af2251cc6c4e
kq333 <afugowski@yandex.com>
2023-05-08 10:07:00 +0200

    changed props name

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -3,11 +3,11 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
 import { Movie } from '../../types/Movie';
 
-interface MovieProps {
+interface MoviesListProps {
   movies: Movie[],
 }
 
-export const MoviesList: React.FC<MovieProps> = ({ movies }) => (
+export const MoviesList: React.FC<MoviesListProps > = ({ movies }) => (
 
   <div className="movies">
     {movies.map(movie => (
