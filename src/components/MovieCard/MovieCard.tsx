8107086d08061ba2dd4c494ba4250c2300d4143a
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2023-05-11 20:53:55 +0300

    Solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
 import './MovieCard.scss';
+import { Movie } from '../../types/Movie';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
+type Props = {
+  dataMovie: Movie;
+};
+
+export const MovieCard: React.FC<Props> = ({ dataMovie }) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img data-cy="MovieImage" src={dataMovie.imgUrl} alt="Film logo" />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src="images/imdb-logo.jpeg" alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-8" data-cy="MovieTitle">
+            {dataMovie.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">{dataMovie.description}</p>
+
+        <a href={dataMovie.imdbUrl} data-cy="MovieLink">
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+// eslint-disable-next-line import/no-cycle
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type Props = {
+  dataMovie: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ dataMovie }) => (
+  <div className="movies">
+    {dataMovie.map((movie) => (
+      <MovieCard dataMovie={movie} key={movie.imdbId} />
+    ))}
+  </div>
 );
