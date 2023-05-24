40a343bb846645fdb899b6ed6f6a2d15f16885f6
Victor Buldenko <gookidoo@gmail.com>
2023-05-24 17:22:54 +0300

    test failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.scss';
 import { MovieCard } from './components/MovieCard';
-// import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
       <div className="movies">
-        {/* <MoviesList {...moviesFromServer} /> */}
         {moviesFromServer.map(el => <MovieCard key={el.imdbId} {...el} />)}
       </div>
     </div>
