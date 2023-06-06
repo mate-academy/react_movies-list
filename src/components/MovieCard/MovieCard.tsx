008de4fa574f43b252acf8ce12425ccd68b752bb
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-06-06 14:04:47 +0300

    solution actual version of task

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Movie } from '../../type/Movie';
+
 import './MovieCard.scss';
 
-type Props = Pick<Movie, 'description' | 'title' | 'imdbUrl' | 'imgUrl'>;
+type Props = {
+  title: string,
+  description: string,
+  imgUrl: string,
+  imdbUrl: string
+};
 
 export const MovieCard: React.FC<Props> = ({
   title,
@@ -10,36 +15,53 @@ export const MovieCard: React.FC<Props> = ({
   imgUrl,
   imdbUrl,
 }) => (
-  <>
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img
-            src={imgUrl}
-            alt="Film logo"
-          />
-        </figure>
+  <div
+    data-cy="Movie"
+    className="card"
+  >
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img
+          data-cy="MovieImage"
+          src={imgUrl}
+          alt="Film logo"
+        />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img
+              src="images/imdb-logo.jpeg"
+              alt="imdb"
+            />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p
+            data-cy="MovieTitle"
+            className="title is-8"
+          >
+            {title}
+          </p>
+        </div>
       </div>
 
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <figure className="image is-48x48">
-              <img src="images/imdb-logo.jpeg" alt="imdb" />
-            </figure>
-          </div>
-
-          <div className="media-content">
-            <p className="title is-8">{title}</p>
-          </div>
-        </div>
-
-        <div className="content">
+      <div className="content">
+        <p data-cy="MovieDescription">
           {description}
-          <br />
-          <a href={imdbUrl}>IMDB</a>
-        </div>
+        </p>
+        <br />
+        <a
+          data-cy="MovieLink"
+          href={imdbUrl}
+        >
+          IMDB
+        </a>
       </div>
     </div>
-  </>
+  </div>
 );
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Movie } from '../../type/Movie';
+import { Movie } from '../../types/Movie';
 import { MovieCard } from '../MovieCard';
 import './MoviesList.scss';
 
 type Props = {
-  movies: Movie[];
+  // eslint-disable-next-line react/require-default-props
+  movies?: Movie[];
 };
 
-export const MoviesList: React.FC<Props> = ({ movies }) => (
-  <>
-    <ul>
-      {movies.map(movie => (
-        <li key={movie.imdbId}>
-          <MovieCard {...movie} />
-        </li>
-      ))}
-    </ul>
-  </>
+export const MoviesList: React.FC<Props> = ({ movies = [] }) => (
+  <ul className="movies">
+    {movies.map(({
+      imdbId,
+      title,
+      description,
+      imgUrl,
+      imdbUrl,
+    }) => (
+      <li key={imdbId}>
+        <MovieCard
+          title={title}
+          description={description}
+          imgUrl={imgUrl}
+          imdbUrl={imdbUrl}
+        />
+      </li>
+    ))}
+  </ul>
 );
