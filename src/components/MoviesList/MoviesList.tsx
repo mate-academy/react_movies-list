06b7c121bbe6d2f900406b7fe66ecd65480cdd6b
Roksolana Tylipska <roksolana1407@gmail.com>
2023-06-07 22:44:06 +0300

    Add key

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} />
+      <MovieCard key={movie.imdbId} movie={movie} />
     ))}
     ;
   </div>
