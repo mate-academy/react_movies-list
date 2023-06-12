2146a6eee78f445f703b523ce5fa041ede9f7598
Viacheslav Chervinko <chervinkoslavik@gmail.com>
2023-06-12 10:26:55 +0300

    remove unnecessary react fragment

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,13 @@ import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList/MoviesList';
 
 export const App: React.FC = () => (
-  <>
-    <div className="page">
-      <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
-      </div>
+  <div className="page">
+    <div className="page-content">
+      <MoviesList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
     </div>
-  </>
+  </div>
 );
