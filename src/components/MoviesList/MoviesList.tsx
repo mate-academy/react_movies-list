454cfe33bf1bc8aa38bc4ac9ff95198653ae90af
AndriiYelieva <andrii.yelieva@gmail.com>
2023-06-18 18:04:11 +0300

    add task

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
 import './MoviesList.scss';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+import { MovieCard } from '../MovieCard';
+
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  movies: Movie[];
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <div key={movie.imdbId} className="card" data-cy="Movie">
+        <MovieCard movie={movie} />
+      </div>
+    ))}
+  </div>
 );
