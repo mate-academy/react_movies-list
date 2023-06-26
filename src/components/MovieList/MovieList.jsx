d2ad6d877a99fe1e53fdab27459763a97e94dbc1
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-06-26 23:19:22 +0300

    Task resolved

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,14 @@
+import { MovieCard } from '../MovieCard';
 import './MovieList.scss';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <>
+    <div className="movies">
+      {
+        movies.map(movie => (
+          <MovieCard movie={movie} key={movie.imdbId} />
+        ))
+      }
+    </div>
+  </>
 );
