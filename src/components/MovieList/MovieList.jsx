295dd9bf64dbce4be1e1b643756c6db5feafeb6a
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-06-27 10:16:40 +0300

    solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,10 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => (
-  <>Put the list here</>
+export const MovieList = ({ movies }) => (
+  <ul className="movies">
+    {movies.map(movie => (
+      <MovieCard movie={movie} />
+    ))}
+  </ul>
 );
