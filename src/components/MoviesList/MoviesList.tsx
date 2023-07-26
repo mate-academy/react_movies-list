c501e70e60749935020505269a7c099c37c82210
Nazar Yants <nazyants@gmail.com>
2023-07-26 13:44:21 +0300

    fix after review

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -11,9 +11,7 @@ type Props = {
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <div key={movie.imdbId} className="card" data-cy="Movie">
-        <MovieCard movie={movie} />
-      </div>
+      <MovieCard movie={movie} />
     ))}
   </div>
 );
