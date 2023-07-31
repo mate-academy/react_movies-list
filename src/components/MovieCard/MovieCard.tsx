c0a1c3e31e1e220362fdb46de390bd4b9eb632ad
domkab <dominykas.bobinas@gmail.com>
2023-07-31 18:31:40 +0300

    1.1.0 destructured movie

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -6,14 +6,21 @@ import { Movie } from '../../types/Movie';
 interface MovieCardProps {
   movie: Movie;
 }
-export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => (
+export const MovieCard: React.FC<MovieCardProps> = (
+  {
+    movie:
+    {
+      imgUrl, title, description, imdbUrl,
+    },
+  },
+) => (
   <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
         <img
           data-cy="MovieImage"
-          src={movie.imgUrl}
-          alt={`${movie.title} logo`}
+          src={imgUrl}
+          alt={`${title} logo`}
         />
       </figure>
       <div className="card-content">
@@ -26,18 +33,18 @@ export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => (
 
           <div className="media-content">
             <p className="title is-8" data-cy="MovieTitle">
-              {movie.title}
+              {title}
             </p>
           </div>
         </div>
 
         <div className="content">
           <p data-cy="MovieDescription">
-            {movie.description}
+            {description}
           </p>
 
           <a
-            href={movie.imdbUrl}
+            href={imdbUrl}
             data-cy="MovieLink"
           >
             IMDB
