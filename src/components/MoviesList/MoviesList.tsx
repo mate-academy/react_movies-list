ca6789095a907efc0ddc6882d1592a90eabd6a0a
Alessandra Nitti <alessandrannitti@gmail.com>
2023-08-01 13:14:23 +0200

    done

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -7,11 +7,10 @@ import { Movie } from '../../types/Movie';
 type Props = {
   movies: Movie[];
 };
-
 export const MoviesList: React.FC<Props> = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard movie={movie} />
+      <MovieCard movie={movie} key={movie.imdbId}/>
     ))}
   </div>
 );
