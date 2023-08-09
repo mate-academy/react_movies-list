bc410f11d57963d97150a1374c26cbd013d08920
Hanna Anishyna <ann.986@gmail.com>
2023-08-09 11:02:30 -0700

    fixes

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -7,13 +7,21 @@ type Props = {
   movie: Movie
 };
 
-export const MovieCard: React.FC<Props> = ({ movie }) => (
+export const MovieCard: React.FC<Props>
+= ({
+  movie: {
+    imgUrl,
+    title,
+    imdbUrl,
+    description,
+  },
+}) => (
   <>
     <div className="card-image">
       <figure className="image is-4by3">
         <img
           data-cy="MovieImage"
-          src={movie.imgUrl}
+          src={imgUrl}
           alt="Film logo"
         />
       </figure>
@@ -29,18 +37,18 @@ export const MovieCard: React.FC<Props> = ({ movie }) => (
 
         <div className="media-content">
           <p className="title is-8" data-cy="MovieTitle">
-            {movie.title}
+            {title}
           </p>
         </div>
       </div>
 
       <div className="content">
         <p data-cy="MovieDescription">
-          {movie.description}
+          {description}
         </p>
 
         <a
-          href={movie.imdbUrl}
+          href={imdbUrl}
           target="_blank"
           rel="noreferrer"
           data-cy="MovieLink"
