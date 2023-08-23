50cab6bf542fbc088983c863f89724dfddf80b27
Klaudia <kklis@interia.pl>
2023-08-23 12:47:36 +0200

    Add task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
-);
+type Props = {
+  movies: Array<Movie>;
+};
+
+export const MoviesList: React.FC<Props> = ({ movies }) => {
+  const movieCard = movies.map(movie => {
+    return <MovieCard movie={movie} key={movie.imdbId} />;
+  });
+
+  return (
+    <div className="movies">
+      {movieCard}
+    </div>
+  );
+};
