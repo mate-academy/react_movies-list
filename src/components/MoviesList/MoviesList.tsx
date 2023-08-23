65fc76d1fa48575ed2c1b619f7af6000f748c463
Piotr <piotrkrasucki@gmail.com>
2023-08-23 16:12:39 +0200

    sol

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './MoviesList.scss';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => (
-  <>Put the list here</>
+type PP = {
+  moviesArr: Movie[];
+};
+
+export const MoviesList: React.FC<PP> = ({ moviesArr }) => (
+  <div className="movies">
+    <ul>
+      {moviesArr.map(movie => (
+        <li key={movie.imgUrl}>
+          <MovieCard
+            movie={movie}
+          />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
