1424c94cccb66536800b75f7d210a9b6cd182f86
Andrzej Pietkiewicz <pietan@me.com>
2023-08-23 15:27:46 +0200

    MoviesList solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
-
+import { Movie } from '../../types/Movie';
 import './MovieCard.scss';
 
-export const MovieCard: React.FC = () => (
-  <>Put the card here</>
-);
+type Props = {
+  card: Movie;
+};
+
+export const MovieCard: React.FC<Props> = ({ card }: Props) => {
+  // const {
+  //   imgUrl, title, description, imdbUrl,
+  // } = card;
+
+  return (
+    <div className="card" data-cy="Movie">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            data-cy="MovieImage"
+            src={card.imgUrl}
+            alt="Film logo"
+          />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-left">
+            <figure className="image is-48x48">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </figure>
+          </div>
+
+          <div className="media-content">
+            <p className="title is-8" data-cy="MovieTitle">
+              {card.title}
+            </p>
+          </div>
+        </div>
+
+        <div className="content">
+          <p data-cy="MovieDescription">
+            {card.description}
+          </p>
+
+          <a
+            href={card.imdbUrl}
+            data-cy="MovieLink"
+          >
+            IMDB
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
