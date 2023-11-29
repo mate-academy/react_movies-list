4e8d0d8e2651bfffaf6ce324ce2ad44826f1a546
Wiktoria Bielec <wiktoriabielec3@gmail.com>
2023-11-29 15:48:02 +0100

    Lint changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { MoviesList } from './components/MoviesList';
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
-    <MoviesList movies={moviesFromServer} />
+      <MoviesList movies={moviesFromServer} />
     </div>
     <div className="sidebar" data-cy="Sidebar">
       Sidebar will be here
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,18 +1,18 @@
-
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 import { Movie } from '../../types/Movie';
 
 type Props = {
   movies: Movie[];
-}
+};
 
-export const MoviesList = ({movies}: Props) => (
-    <div className="movies">
-      {movies.map(movie => (
-        <MovieCard key={movie.imdbId}
+export const MoviesList = ({ movies }: Props) => (
+  <div className="movies">
+    {movies.map(movie => (
+      <MovieCard
+        key={movie.imdbId}
         movie={movie}
-        />
-      )) }
-    </div>
+      />
+    )) }
+  </div>
 );
