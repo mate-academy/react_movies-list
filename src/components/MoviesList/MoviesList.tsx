fe4ace1535449102a7b0ca9f5703bb32a89b28b8
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-03-09 10:38:25 +0100

    add task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,4 +1,14 @@
 import React from 'react';
 import './MoviesList.scss';
+import { MoviesListProps } from '../../types/MoviesListProps';
+import { MovieCard } from '../MovieCard';
 
-export const MoviesList: React.FC = () => <>Put the list here</>;
+export const MoviesList: React.FC<MoviesListProps> = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => {
+        return <MovieCard movie={movie} />;
+      })}
+    </div>
+  );
+};
