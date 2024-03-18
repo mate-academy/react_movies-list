17fe2e6954d230e580bd8320725b034e44ee5429
Justin <thedarkvibe1@gmail.com.com>
2024-03-18 13:09:20 +0100

    fix linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'bulma/css/bulma.css';
 
-import { App } from './App';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
