27215bc47a8c686913bd09aae65e41525306e9f3
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-03-20 14:10:07 +0100

    Update App 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
+
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MoviesList } from './components/MoviesList';
