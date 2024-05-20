f42a0f0ca5a226d332c93ed25b6a9e625e4f3bf1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-20 13:49:38 +0200

    task solution

--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import './MoviesList.scss';
+import {Movie} from '../../types/Movie';
+import {MovieCard} from '../MovieCard';
 
-export const MoviesList: React.FC = () => <>Put the list here</>;
+type Props = {
+  movies: Movie[];
+}
+
+export const MoviesList: React.FC<Props> = ({movies}) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard movie={movie} key={movie.imdbId} />
+      ))}
+    </div>
+  );
+};
