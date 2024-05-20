eb7d8e5ba245e5a88cab22a9ab4651b82158f9a5
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-05-20 13:50:20 +0200

    task solution

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 
 import './MovieCard.scss';
-import { Movie } from '../../types/Movie'
+import { Movie } from '../../types/Movie';
 
 export type Props = {
   movie: Movie;
-}
+};
 
-export const MovieCard: React.FC<Props> = ({movie}) => {
+export const MovieCard: React.FC<Props> = ({ movie }) => {
   return (
     <div className="card" data-cy="Movie">
       <div className="card-image">
         <figure className="image is-4by3">
-          <img
-            data-cy="MovieImage"
-            src={movie.imgUrl}
-            alt="Film logo"
-          />
+          <img data-cy="MovieImage" src={movie.imgUrl} alt="Film logo" />
         </figure>
       </div>
 
@@ -36,14 +32,9 @@ export const MovieCard: React.FC<Props> = ({movie}) => {
         </div>
 
         <div className="content">
-          <p data-cy="MovieDescription">
-            {movie.description}
-          </p>
+          <p data-cy="MovieDescription">{movie.description}</p>
 
-          <a
-            href={movie.imdbUrl}
-            data-cy="MovieLink"
-          >
+          <a href={movie.imdbUrl} data-cy="MovieLink">
             IMDB
           </a>
         </div>
--- src/components/MoviesList/MoviesList.tsx
+++ src/components/MoviesList/MoviesList.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './MoviesList.scss';
-import {Movie} from '../../types/Movie';
-import {MovieCard} from '../MovieCard';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
 
 type Props = {
   movies: Movie[];
-}
+};
 
-export const MoviesList: React.FC<Props> = ({movies}) => {
+export const MoviesList: React.FC<Props> = ({ movies }) => {
   return (
     <div className="movies">
       {movies.map(movie => (
