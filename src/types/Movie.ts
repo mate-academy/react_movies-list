0e05e9739e59b109b901a2cebf9b80c9406a0593
Wioleta Lip <wiolip@hotmail.com>
2026-01-30 20:15:42 +0100

    initial comit

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,5 +1,7 @@
 export interface Movie {
-  title: string;
-
-  // add all the other field
+  imdbId: number.required;
+  title: string.required;
+  description: string.required;
+  imgUrl: string.required;
+  imdUrl: string.required;
 }
